5891b53536bd0f9d75e5151e49a06a3d2c096b72
Yejin Shin <shinyejin0212@gmail.com>
2022-10-10 16:56:35 +0900

    [BOJ] 음계/브론즈2/15분

--- 구현/boj_2920음계.py
+++ 구현/boj_2920음계.py
@@ -5,8 +5,8 @@
 # 둘 다 아니면 함수 중단 mixed 출력. (무조건 mixed이기 때문) 으로 기본 설정
 # 양쪽 끝 값을 더해서 9가 나오지 않는 순간이 있으면 mixed로 판별
   ## 이로 인해 모든 값을 for문으로 돌리지 않아도 되므로 시간 단축 예상..?
-
-array = list(map(int, input().split()))
+## 어디가 틀린 건지 모르겠다... ..
+array = list(map(int, input().split(' ')))
 
 def scale (array) :
     if array[0] == 1:
@@ -17,8 +17,8 @@ def scale (array) :
         result = 'mixed'
         return result
 
-    for i in range(0,7) :
-        if array[i] + array[7-i] == 9:
+    for i in range(1,8) :
+        if array[i] + array[8-i] == 9:
             continue
         else :
             result = 'mixed'
