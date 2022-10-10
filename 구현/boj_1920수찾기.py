97701239f839d24788ac4c517ad4e2234cc8687f
Yejin Shin <shinyejin0212@gmail.com>
2022-10-10 21:10:27 +0900

    [BOJ] 수찾기/실버4/15분

---
+++ 구현/boj_1920수찾기.py
@@ -0,0 +1,31 @@
+# 해시, 배열, 구현
+# 하, 20분
+# 배열로 풀었더니 시간초과
+# set 선언만 해주면 통과됨
+
+n = int(input())
+arr1 = list(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for i in arr2 :
+    if i in arr1:
+        print('1')
+    else:
+        print('0')
+
+# 정답
+# set 자료형은 중복을 방지함
+
+# n = int(input())
+# arr1 = set((map(int, input().split()))
+
+# m = int(input())
+# arr2 = list(map(int, input().split()))
+
+# for i in arr2 :
+#     if i in arr1:
+#         print('1')
+#     else:
+#         print('0')
