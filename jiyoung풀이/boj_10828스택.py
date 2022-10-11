51001cdbb2e5d36f3c37497bf2aae5a780a5d5bc
이지영 <wldudwldhd@naver.com>
2022-10-11 18:39:54 +0900

    [BOJ]스택/실버4/4분소요
    https://www.acmicpc.net/problem/10828

---
+++ jiyoung풀이/boj_10828스택.py
@@ -0,0 +1,26 @@
+# 문제 스스로 풀어보기
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    command = input().split()
+    
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "top":
+        if len(stack) != 0: 
+            print(stack[-1])
+        else: print(-1)
+    elif command[0] == "size":
+            print(len(stack))
+    elif command[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else :
+            print(stack.pop())
+    elif command[0] == "empty": 
+        if len(stack) == 0 : print(1)
+        else : print(0)
