9b04011064d4da8a5cf88e7641983fb586c8b3f5
이지영 <wldudwldhd@naver.com>
2022-10-15 22:56:52 +0900

    [BOJ]mixingmilk/브론즈2/23분 소요

---
+++ jiyoung풀이/boj_16769mixingmilk.py
@@ -0,0 +1,26 @@
+# 문제 스스로 풀어보기
+
+# the candy war과 비슷하다고 느낌
+
+import sys
+input = sys.stdin.readline
+
+# capcity, amount
+lst = []
+for _ in range(3):
+    c, a = map(int, input().split())
+    lst.append([c,a])
+
+for i in range(100):
+    if lst[i%3][1] + lst[(i+1)%3][1] <= lst[(i+1)%3][0]:
+        lst[(i+1)%3][1] += lst[i%3][1]
+        lst[i%3][1] = 0
+    else:
+        # 다음 양동이의 양과 현재 양동이의 양을 더한 값이, 다음양동이의 용량을 초과하면
+        temp = lst[(i+1)%3][0] - lst[(i+1)%3][1]
+        lst[(i+1)%3][1] += temp
+        lst[i%3][1] -= temp
+
+for i in range(3):
+    print(lst[i][1])
+
