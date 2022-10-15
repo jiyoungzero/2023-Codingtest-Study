a2791a25e2d206e6f07c7ab6c7b317ae3b32f445
Yejin Shin <shinyejin0212@gmail.com>
2022-10-15 21:19:29 +0900

    17269 & 17224 수정

--- shynnn/그리디/boj_17224APC는왜서브태스크대회가되었을까.py
+++ shynnn/그리디/boj_17224APC는왜서브태스크대회가되었을까.py
@@ -1,32 +1,26 @@
 # 브론즈1
 # 20분
-
-from re import L
-
+# hard 태스크를 최대한 많이 풀어야함
 
 n, l, k = map(int, input().split())
-
-if (n < 1 and n > 100) or (k < 0 and k > n) or (l < 1 and l > 50):
-    n, l, k = map(int, input().split())
-
 score = 0
-
-exam = []
-for i in range(n):
-    exam.append([])
-    a, b = map(int,  input().split())
-
-    exam[i].append(a)
-    exam[i].append(b)
-exam.sort(key=lambda x: (x[1], x[0]), reverse=True)
-
+hard, easy = 0, 0
 cnt = 0
 for i in range(n):
-    if cnt < k:
-        if exam[i][1] <= l:
-            score += 140
-        elif exam[i][0] <= l:
-            score += 100
-    else:
-        break
+    sub1, sub2 = map(int,  input().split())
+    if sub1 <= l:
+        # score += 140
+        hard += 1
+    elif sub2 <= l:
+        # score += 100
+        easy += 1
+
+
+# sort할 필요 없음
+# exam.sort(key=lambda x: (x[1], x[0]), reverse=True)
+
+score = min(hard, k) * 140
+
+if hard < k:
+    score += min(k-hard, easy) * 100
 print(score)
