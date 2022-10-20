85b2373b422ec1ab07d131eb4e67b5c4c8a3667b
이지영 <wldudwldhd@naver.com>
2022-10-20 23:02:14 +0900

    [BOJ]가장큰증가부분수열/실버2/재풀이필요

---
+++ jiyoung풀이/boj_11055가장큰증가부분수열.py
@@ -0,0 +1,20 @@
+# 문제 스스로 풀어보기 
+
+# 중요한 문제임.!
+
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+array=list(map(int, input().split()))
+
+d=[1]*n
+d[0]=array[0]
+for i in range(1,n):
+  for j in range(i):
+    if array[j]<array[i]:
+      d[i]=max(d[i], d[j]+array[i])
+    else:
+      d[i]=max(d[i], array[i])
+
+print(max(d))
