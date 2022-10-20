d19e11c90444c401863b69b4de357255a8fb7ed1
Yejin Shin <shinyejin0212@gmail.com>
2022-10-20 14:57:39 +0900

    12100수정

--- shynnn/DFS_BFS/boj_12100_2048Easy.py
+++ shynnn/DFS_BFS/boj_12100_2048Easy.py
@@ -17,42 +17,45 @@
 
 # 1.rotate 사용해 Map 돌리기 풀이
 # 1. DFS + rotate 사용해 Map 돌리기 풀이
-from copy import deepcopy
+# from copy import deepcopy
 
 
-def rotate90(b, n):
-    new_b = deepcopy(b)
-    for i in range(n):
-        for j in range(n):
-            new_b[n-1-j][i] = b[i][j]  # 외우기. 90도 돌리는 것!
-    return new_b
+# def rotate90(b, n):
+#     new_b = deepcopy(b)
+#     for i in range(n):
+#         for j in range(n):
+#             new_b[n-1-j][i] = b[i][j]  # 외우기. 90도 돌리는 것!
+#     return new_b
 
 
-def convert(lst):
-    newlst = [i for i in lst if i]  # lst 안의 양수인 값만 넣은 newlst
-    for i in range(len(newlst)-1):  # -1해줄것. i+1과 비교하는 것이기 때문에 경계값
-        if newlst[i] == newlst[i+1]:
-            newlst[i] = newlst[i]*2
-            newlst[i+1] = 0
-    lst = [i for i in lst if i]  # 다시한번 양수인 값만 넣기
-    return newlst + [0] * (len(lst)-len(newlst))
+# def convert(lst):
+#     newlst = [i for i in lst if i]  # lst 안의 양수인 값만 넣은 newlst
+#     for i in range(len(newlst)-1):  # -1해줄것. i+1과 비교하는 것이기 때문에 경계값
+#         if newlst[i] == newlst[i+1]:
+#             newlst[i] = newlst[i]*2
+#             newlst[i+1] = 0
+#     lst = [i for i in lst if i]  # 다시한번 양수인 값만 넣기
+#     return newlst + [0] * (len(lst)-len(newlst))
 
 
-def dfs(n, b, cnt):
-    ret = max([max(i) for i in b])
-    if cnt == 0:  # 5번 다 움직였을 경우
-        return ret  # return
-    for _ in range(4):
-        x = [convert(i) for i in b]
-        if b != x:  # 같을 경우에는 더이상 변하지 않는거니까 dfs를 할 필요 없음
-            ret = max(ret, dfs(n, x, cnt-1))
-        b = rotate90(b, n)  # 돌려서 다시. 총 4번
-    return ret
+# def dfs(n, b, cnt):
+#     ret = max([max(i) for i in b])
+#     if cnt == 0:  # 5번 다 움직였을 경우
+#         return ret  # return
+#     for _ in range(4):
+#         x = [convert(i) for i in b]
+#         if b != x:  # 같을 경우에는 더이상 변하지 않는거니까 dfs를 할 필요 없음
+#             ret = max(ret, dfs(n, x, cnt-1))
+#         b = rotate90(b, n)  # 돌려서 다시. 총 4번
+#     return ret
 
 
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
+# n = int(input())
+# board = [list(map(int, input().split())) for _ in range(n)]
 
-print(dfs(n, board, 5))
+# print(dfs(n, board, 5))
 
 # ---------------------------------------------
+# 2. bfs로 하기
+# https://latte-is-horse.tistory.com/349 참고
+# 못하겠음~!
