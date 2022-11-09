29c7c3e190e7c27e4ddb7bd033addf6a0d57e5c8
Yejin Shin <shinyejin0212@gmail.com>
2022-11-09 22:37:59 +0900

    refactor : 1959두개의숫자열

--- shynnn/구현/swea_1959두개의숫자열.py
+++ shynnn/구현/swea_1959두개의숫자열.py
@@ -1,12 +1,24 @@
 # D2
 
-T = int(input())
+# T = int(input())
 
-for t in range(1, T+1):
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
+# for t in range(1, T+1):
+#     N, M = map(int, input().split())
+#     A = list(map(int, input().split()))
+#     B = list(map(int, input().split()))
 
+#     large = A if N >= M else B
+#     small = A if large == B else B
+
+#     result = 0
+#     for i in range(len(large)-len(small)+1):
+#         temp = 0
+#         for j in range(len(small)):
+#             temp += large[i+j] * small[j]
+#         result = max(result, temp)
+#     print("#{} {}".format(t, result))
+
+def calculate(large, N, small, M):
     large = A if N >= M else B
     small = A if large == B else B
 
@@ -16,4 +28,20 @@ for t in range(1, T+1):
         for j in range(len(small)):
             temp += large[i+j] * small[j]
         result = max(result, temp)
-    print("#{} {}".format(t, result))
+        return result
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    if N > M:
+        ans = calculate(A, N, B, M)
+
+    else:
+        ans = calculate(B, M, A, N)
+
+    print("#{} {}".format(t, ans))
