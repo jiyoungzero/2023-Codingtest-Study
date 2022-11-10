51777a518ecaee1b02ebfc608bfc9d57c34603d4
이지영 <wldudwldhd@naver.com>
2022-11-10 19:44:57 +0900

    [SWEA]가능한시험점수/D4/30분초과

--- jiyoung풀이/swea_3752가능한시험점수.py
+++ jiyoung풀이/swea_3752가능한시험점수.py
@@ -2,24 +2,34 @@
 
 import sys
 input = sys.stdin.readline
-import itertools
+# import itertools
 
 T = int(input().rstrip())
 
 for t in range(1, T+1):
-    result = set()
-    
     n = int(input())
     prob = list(map(int, input().split()))
     
-    # for i in range(n):
-    #     for j in range(i, n):
-    #         if sum(prob[i:j]) not in result:
-    #             result.add(sum(prob[i:j]))
-    for i in range(n+1):
-        c_prob = itertools.combinations(prob, i)
-        for c in list(c_prob):
-            result.add(sum(c))
-
-    print(result)
+    result = [0] # 초기화 
+    
+    for p in prob:
+        result = list(set(result)) # 중복 제거 
+        for i in range(len(result)):
+            result.append(result[i] + p)
+            
+    result = set(result)
     print(f"#{t} {len(result)}")
+
+# 런타임 에러
+# for t in range(1, T+1):
+#     result = set()
+    
+#     n = int(input())
+#     prob = list(map(int, input().split()))
+    
+#     for i in range(n+1):
+#         c_prob = itertools.combinations(prob, i) # 조합의 경우의 수
+#         for c in list(c_prob):
+#             result.add(sum(c))
+
+#     print(f"#{t} {len(result)}")
