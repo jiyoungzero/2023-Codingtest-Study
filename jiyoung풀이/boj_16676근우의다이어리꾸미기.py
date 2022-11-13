8eb94ba6498e759db70a4f00eaab51942d04c503
이지영 <wldudwldhd@naver.com>
2022-11-13 20:58:51 +0900

    [BOJ]근우의다이어리꾸미기/브론즈2/실패

---
+++ jiyoung풀이/boj_16676근우의다이어리꾸미기.py
@@ -0,0 +1,26 @@
+# 문제 스스로 풀어보기 : 10분 --> 테스트 케이스 중에 뭐가 안맞는 건지 모르겠음 
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+str_n = str(n)
+result = 0 
+num_lst = []
+# 284라고 하면 222가 필요하니까 3개 
+for i in range(len(str_n)):
+    tmp = []
+    # 각 자리의 수보다 작은 숫자 리스트생성 
+    # 각자의 가장 작은 숫자가 공통으로 몇개 들어있는 cnt == result 
+    for i in range(int(str_n[i])+1):
+        tmp.append(i)
+    num_lst.append(tmp)
+    
+for i in range(len(str_n)):
+    if 1 in num_lst[i]: # 각자의 가장 작은 숫자는 1(0이 아님)
+        result += 1
+print(result) 
+    
+
+    
+    
