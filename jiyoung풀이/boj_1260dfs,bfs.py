f72f83149f2134b83b48e121773d0855f4ca2352
이지영 <wldudwldhd@naver.com>
2022-12-21 01:13:19 +0900

    [BOJ]dfs,bfs/실버2/재풀이2

--- jiyoung풀이/boj_1260dfs,bfs.py
+++ jiyoung풀이/boj_1260dfs,bfs.py
@@ -1,38 +1,36 @@
-# 기본 개념 문제 dfs, bfs 
-# dfs : 깊이, bfs : 너비 
 from collections import deque
 import sys
-input = sys.stdin.readline
+read = sys.stdin.readline
 
-n, m, v = map(int, input().split())
-graph = [[0]*(n+1) for _ in range(n+1)]
-
-visited1 = [0] * (n+1)
-visited2 = [0] * (n+1)
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
-    
-
-# dfs : 재귀의 성질 
-def dfs(v):
-    visited1[v] = 1
-    print(v, end=" ")
-    for i in range(1, n+1):
-        if visited1[i] == 0 and graph[v][i] == 1:
-            dfs(i) # 재귀
-
-
-# bfs : 큐의 성질 
 def bfs(v):
     q = deque()
-    q.append(v)
-    visited2[v] = 1
+    q.append(v)       
+    visit_list[v] = 1   
     while q:
         v = q.popleft()
-        print(v, end= " ")
-        for i in range(1, n+1):
-            if visited2[v] == 0 and graph[v][i] == 1:
+        print(v, end = " ")
+        for i in range(1, n + 1):
+            if visit_list[i] == 0 and graph[v][i] == 1:
                 q.append(i)
-                visited2[i] = 1
+                visit_list[i] = 1
+
+def dfs(v):
+    visit_list2[v] = 1        
+    print(v, end = " ")
+    for i in range(1, n + 1):
+        if visit_list2[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+
+n, m, v = map(int, read().split())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)] 
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, read().split())
+    graph[a][b] = graph[b][a] = 1
+
+dfs(v)
+print()
+bfs(v)
