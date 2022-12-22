b9d9fa33185cdba0feba944e56ad01a27cb6ed8e
이지영 <wldudwldhd@naver.com>
2022-12-22 23:56:32 +0900

    [BOJ]BFS,DFS/실버2/재풀이3

--- jiyoung풀이/boj_1260dfs,bfs.py
+++ jiyoung풀이/boj_1260dfs,bfs.py
@@ -1,35 +1,36 @@
 from collections import deque
 import sys
-read = sys.stdin.readline
+input = sys.stdin.readline
 
-def bfs(v):
-    q = deque()
-    q.append(v)       
-    visit_list[v] = 1   
-    while q:
-        v = q.popleft()
-        print(v, end = " ")
-        for i in range(1, n + 1):
-            if visit_list[i] == 0 and graph[v][i] == 1:
-                q.append(i)
-                visit_list[i] = 1
 
-def dfs(v):
-    visit_list2[v] = 1        
-    print(v, end = " ")
-    for i in range(1, n + 1):
-        if visit_list2[i] == 0 and graph[v][i] == 1:
-            dfs(i)
-
-n, m, v = map(int, read().split())
-
-graph = [[0] * (n + 1) for _ in range(n + 1)] 
-visit_list = [0] * (n + 1)
-visit_list2 = [0] * (n + 1)
+n,m,v = map(int, input().split())
+graph = [[0]*(n+1) for i in range(n+1)]
 
 for _ in range(m):
-    a, b = map(int, read().split())
+    a,b = map(int, input().split())
     graph[a][b] = graph[b][a] = 1
+    
+visited1 = [0] * (n+1)
+visited2 = [0] * (n+1)
+
+def dfs(v):
+    visited1[v] = 1
+    print(v, end=" ")
+    for i in range(n+1):
+        if visited1[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+    
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited2[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in range(n+1):   
+            if visited2[i] == 0 and graph[v][i] == 1:
+                visited2[i] = 1
+                q.append(i)
 
 dfs(v)
 print()
