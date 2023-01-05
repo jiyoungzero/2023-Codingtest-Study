0722242ba902afe74883a59a47625ca35602aa16
이지영 <wldudwldhd@naver.com>
2023-01-05 23:51:09 +0900

    [이코테]미래도시/230105/재풀이필요

--- jiyoung풀이/book_미래도시.py
+++ jiyoung풀이/book_미래도시.py
@@ -3,3 +3,11 @@
 import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+    
+print(graph)
