28d1570f5caa71d5182498a57ca6fce4807659e6
이지영 <wldudwldhd@naver.com>
2023-01-08 17:28:53 +0900

    [이코테]모험가길드/230108/모범답안포함/try1

--- jiyoung풀이/book_모험가길드.py
+++ jiyoung풀이/book_모험가길드.py
@@ -6,15 +6,28 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr.sort(reverse=True) # 내림차순
-cnt = 0
+# arr.sort(reverse=True) # 오름차순으로 해야 최대그룹수가 나옴!!!
+# cnt = 0
 
-while arr:
-    c = arr[0]
-    for _ in range(c): # 3 2 2 2 1 
-        del arr[0]
-    cnt += 1
+# while arr:
+#     c = arr[0]
+#     for _ in range(c): # 1 2 2 2 3
+#         del arr[0]
+#     cnt += 1
     
-print(cnt)
+# print(cnt)
+
+# 모범답안
+arr.sort() # 오름차순 정렬
+
+result = 0
+cnt = 0
+for ele in arr:
+    cnt += 1
+    if cnt >= ele:
+        result += 1 # 현재 모집된 인원이 공포도 이상이라면 그룹 형성
+        cnt = 0
+print(result)        
+    
 
 
