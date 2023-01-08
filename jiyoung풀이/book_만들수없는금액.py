576235f1c06e97296b554fce773b0b79d3550909
이지영 <wldudwldhd@naver.com>
2023-01-09 02:30:56 +0900

    [이코테]만들수없는금액/230109/모범풀이다시/try1

--- jiyoung풀이/book_만들수없는금액.py
+++ jiyoung풀이/book_만들수없는금액.py
@@ -12,17 +12,23 @@ arr = list(map(int, input().split()))
 
 arr.sort() # 오름차순
 
-# 1 1 2 3 9 
-result = 0
-v = [False]*(1001)
-v[0] =True 
+# 1 1 2 3 9  --> 수평선에 범위 그려가면서 보면 됨
+# result = 0
+# v = [False]*(1001)
+# v[0] =True 
 
-for i in range(1,len(arr)):
-    combi = list(combinations(arr, i))
-    for ele in combi:
-        v[sum(ele)] = True
+# for i in range(1,len(arr)):
+#     combi = list(combinations(arr, i))
+#     for ele in combi:
+#         v[sum(ele)] = True
     
-for (i, value) in enumerate(v):
-    if value == False:
-        print(i)
-        break
+# for (i, value) in enumerate(v):
+#     if value == False:
+#         print(i)
+#         break
+target = 1
+for ele in arr:
+    if ele <= target:
+        target += ele
+    else:break
+print(target)
