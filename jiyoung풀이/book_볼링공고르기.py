87329271f0d6ac7e96fdac8a6582e5ea94dbb35c
이지영 <wldudwldhd@naver.com>
2023-01-09 23:50:39 +0900

    [이코테]볼링공고르기/230109/모범답안/try1

--- jiyoung풀이/book_볼링공고르기.py
+++ jiyoung풀이/book_볼링공고르기.py
@@ -7,9 +7,26 @@ n, m = map(int, input().split()) # n: 볼링 수, m : 공의 최대 무게
 arr = list(map(int, input().split()))
 cnt = 0
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-        if arr[i] == arr[j]:continue
-        else:
-            cnt += 1
-print(cnt)
+# # 내풀이 -> 구현 쪽
+# for i in range(len(arr)):
+#     for j in range(i+1, len(arr)):
+#         if arr[i] == arr[j]:continue
+#         else:
+#             cnt += 1
+# print(cnt)
+
+# 그리디하게 풀이
+arr.sort()# 1 2 2 3 3
+w = [0] * (11) # 최대무게 한도는 10
+
+for ele in arr:
+    w[ele] = arr.count(ele)
+    
+result = 0
+for i in range(m+1):
+    if w[i] >= 1:
+        n -= w[i]
+        result += (n * w[i])
+print(result)        
+    
+
