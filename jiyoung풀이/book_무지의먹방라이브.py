15cad602f1b9cf86bbf9c076adfa3d6edd97a4ba
이지영 <wldudwldhd@naver.com>
2023-01-11 23:28:35 +0900

    [이코테]무지의먹방라이브/230111/try1/재풀이필요

---
+++ jiyoung풀이/book_무지의먹방라이브.py
@@ -0,0 +1,25 @@
+# greedy
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/42891
+import heapq
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1 # 더 섭취할 것이 없으면
+    
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간):(음식 번호)
+        heapq.heappush(q, (food_times[i], i+1))
+    
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    length = len(food_times) # 남은 음식 수
+    while sum_value + ((q[0][0]-previous)*length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now-previous) * length
+        length -= 1
+        previous = now
+        
+    # 음식 번호 기준으로 정렬
+    result = sorted(q, key=lambda x:x[1])
+    return result[(k-sum_value)%length][1]
