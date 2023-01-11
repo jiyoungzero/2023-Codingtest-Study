f89637cd16c5487b769764c001c3acb7c24f71e8
이지영 <wldudwldhd@naver.com>
2023-01-11 23:26:32 +0900

    [이코테]문자열재정렬/230111/try1/isalpha(), *array, ''.join(arr)

--- jiyoung풀이/book_문자열재정렬.py
+++ jiyoung풀이/book_문자열재정렬.py
@@ -8,9 +8,16 @@ num_arr = []
 str_arr = []
 
 for c in command:
-    if 65 <= ord(c) <= 90 or 97<= ord(c) <= 122:
+    # if 65 <= ord(c) <= 90 or 97<= ord(c) <= 122: 
+    
+    ############ 한줄로 판별하기 ################
+    if c.isalpha():
         str_arr.append(c)
     else:
         num_arr.append(int(c))
     str_arr.sort(key=lambda x:ord(x))
-print(*str_arr, sum(num_arr))
+# print(*str_arr, sum(num_arr)) --> 코너 케이스 고려 안함!!
+
+if sum(num_arr) != 0:
+    str_arr.append(str(sum(num_arr)))
+print(''.join(str_arr))
