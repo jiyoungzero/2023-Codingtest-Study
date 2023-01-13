124fc00672d8262126047eae422de7f687c7bd98
이지영 <wldudwldhd@naver.com>
2023-01-13 14:52:26 +0900

    [BOJ]유기농배추/1012/DFS(재귀)/20분

--- jiyoung풀이/boj_1012유기농배추.py
+++ jiyoung풀이/boj_1012유기농배추.py
@@ -1,49 +1,47 @@
 # 문제 스스로 풀어보기
 
 # dfs 인접한 곳들의 뭉탱이 세기  한개 주변 보고 그 주변도 보고..--> 그러면 재귀가 필요함 
-
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000) # dfs의 재귀부분의 탈출 조건이 없어서 재귀의 깊이를 제한해야 런타임에러가 안남 
+input = sys.stdin.readline 
+sys.setrecursionlimit(10000)
 
-test_case = int(input())
-dxs, dys = [0,0,1,-1],[1,-1,0,0]
-
-def in_range(x,y):
-    return 0<=x<r and 0<=y<c
+testcase = int(input())
+dxs, dys = [0,0,1,-1], [1,-1,0,0]
 
 
-# 한 뭉탱이 계산하는 곳 --> 재귀로 해결 
-def dfs(x, y):
-    for i in range(4):
-        nx = x+dxs[i]
-        ny = y+dys[i]
-        if in_range(nx,ny) and lst[nx][ny] == 1 and visited[nx][ny] == False:
-            visited[nx][ny] = True
-            dfs(nx,ny)
-            
-            
+def in_range(x, y):
+    return 0<=x<n and 0<=y<m
 
+# 뭉탱이 개수 세기     
+def dfs(x, y): 
+    for k in range(4):
+        nx, ny = x + dxs[k], y + dys[k]
+        if in_range(nx,ny) and arr[nx][ny] == 1:
+            arr[nx][ny] = 2 # 방문처리    
+            dfs(nx, ny) # 상하좌우로 또 dfs 검사(뭉탱이니까)
 
-for _ in range(test_case):
-    c, r, cnt = map(int, input().split())
-    lst = [[0]*c for _ in range(r)]
-    visited = [[False]*c for _ in range(r)]
+for _ in range(testcase):
+    m, n, k = map(int, input().split()) # m : 열의 개수, n:행의 개수, k:배추 위치
+    arr = [[0]*(m) for _ in range(n)] # 0 : 빈칸, 1: 배추 위치, 2: 방문한 배추 
     result = 0
     
-    # 배추 심기
-    for i in range(cnt):
+    for _ in range(k):
         a, b = map(int, input().split())
-        lst[b][a] = 1 
+        arr[b][a] = 1 
     
-    # 탐색 
-    for i in range(r):
-        for j in range(c):
-            if lst[i][j] == 1 and visited[i][j] == False: 
-                visited[i][j] = True
-                dfs(i,j) # 이거 끝나면 뭉탱이 연산 끝난 거 
+    for i in range(n):
+        for j in range(m):
+            # arr == 1 이면 dfs 돌리기
+            if arr[i][j] == 1:
+                arr[i][j] = 2
+                dfs(i, j)
                 result += 1
     print(result)
+
+                    
+    
+
+
 
         
         
