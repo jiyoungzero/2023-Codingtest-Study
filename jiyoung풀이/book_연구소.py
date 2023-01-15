60493a5fc44cf64e225731c6d70a5e2479d57b97
이지영 <wldudwldhd@naver.com>
2023-01-16 01:48:11 +0900

    [이코테]연구소/230116/모범답안/재풀이필요/try1

--- jiyoung풀이/book_연구소.py
+++ jiyoung풀이/book_연구소.py
@@ -6,54 +6,61 @@ input =sys.stdin.readline
 
 # 0 : 빈칸, 1: 벽, 2: 바이러스, wall = 3 --> 완탐으로 탐색
 n, m = map(int, input().split())
-arr = []
-dxs, dys = [0,0,1,-1],[1,-1,0,0]
 wall = 0
-# 연구실 배열
+data = []
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
+    data.append(list(map(int, input().split())))
     
-#빈칸 배열
-none = []
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 0:
-            none.append((i,j))
-            
-# 벽을 세울 수 있는 조합 리스트
-none_combi = list(combinations(none, 3))
+# 벽을 설치하고 난 배열
+temp = [[0]*m for _ in range(n)]
+dxs, dys = [0,0,1,-1],[1,-1,0,0]
 
-# 바이러스 배열
-virus_arr = []
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 2:
-            virus_arr.append((i, j))
-            
 def in_range(x, y):
     return 0<=x<n and 0<=y<m
 
-def dfs(x, y):
-    if arr[x][y] == 2:
-        for i in range(4):
-            nx, ny = x+dxs[i], y+dys[i]
-            if in_range(nx,ny) and arr[nx][ny] == 0:
-                arr[nx][ny] = 2
-                dfs(nx, ny)
+def virus(x, y):
+    for i in range(4):
+        nx, ny = x+dxs[i], y+dys[i]
+        if in_range(nx, ny) and temp[nx][ny] == 0:
+            temp[nx][ny] = 2
+            virus(nx,ny)
+            
+result = 0
+def dfs(wall):
+    global result
+    if wall == 3: # 울타리 다 설치하고 바이러스 검사
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+        
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+        result = max(result, get_score())
+        return 
+    
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                wall += 1
+                dfs(wall)
+                # reset
+                data[i][j] = 0
+                wall -= 1
                 
-result = -1
-for combi in none_combi:
-    for x, y in combi:
-        arr[x][y] = 1
-        
-    for x, y in virus_arr:
-        dfs(x,y)
-    result = max(result, (arr.count(0)))
-    # reset
-    for x, y in combi:
-        arr[x][y] = 0
-        
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+    
+dfs(0)
 print(result)
-
+    
+            
     
     
