1f8c0610bb87a9701783aa808ee5c923e429801b
이지영 <wldudwldhd@naver.com>
2023-01-17 02:04:41 +0900

    [이코테]경쟁적전염/230117/모범답안/bfs/try1

--- jiyoung풀이/book_경쟁적전염.py
+++ jiyoung풀이/book_경쟁적전염.py
@@ -1,47 +1,41 @@
-# dfs
+# dfs --> 낮은 바이러스 부터 증식 : bfs의 아이디어!
 
 
 # 동시다발적으로 전파 + 전파 시 우선순위가 있을 경우
 
 import sys
 import copy
+from collections import deque
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-    
-s, x, y = map(int, input().split())
+graph = []
+data = [] # 바이러스 관련 정보 
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] != 0:
+            data.append((graph[i][j],0,  i, j) ) # 바이러스 종류, 시간, 위치
+            
+data.sort() # 바이러스 종류를 오름차순으로 정렬
+q = deque(data) # 바이러스 배열에 대한 큐 생성
+
+target_s, target_x, target_y = map(int, input().split())
 dxs, dys = [0,0,1,-1], [1,-1,0,0]
 
-# 이미 있는 바이러스와 비교를 위해
-real_arr = copy.deepcopy(arr)
-# 바이러스 후의 배열
-temp = copy.deepcopy(arr)
-
-time = 0
 def in_range(x, y):
     return 0<=x<n and 0<=y<n
 
-def virus(x, y):
-    for i in range(4):
-        nx, ny = x +dxs[i], y+dys[i]
-        if (in_range(nx, ny) and temp[nx][ny] == 0) or (in_range(nx, ny) and temp[nx][ny] > arr[x][y]):
-            if real_arr[nx][ny] == 0: temp[nx][ny] = arr[x][y]
-            
-def dfs(time):
-    if time == s:return
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] != 0:
-                virus(i, j)
-    time += 1
-    for i in range(n):
-        for j in range(n):
-            arr[i][j] = temp[i][j]
-    dfs(time)
-                
+while q:
+    virus, s, x, y = q.popleft()
+    if s == target_s:
+        break
 
-dfs(time)
-print(temp[x-1][y-1])
+    for k in range(4):
+        nx,ny = x+dxs[k], y+dys[k]
+        if in_range(nx, ny) and graph[nx][ny] == 0:
+            graph[nx][ny] = virus
+            q.append((virus, s+1, nx, ny))
+print(graph[target_x-1][target_y-1])
+            
