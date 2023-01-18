cb04bd553fd8fe46510b429ded846fd9b6eb2c2c
이지영 <wldudwldhd@naver.com>
2023-01-18 20:37:08 +0900

    [이코테]안테나/230118/수정_중간값_재풀이필요/try1

--- jiyoung풀이/book_안테나.py
+++ jiyoung풀이/book_안테나.py
@@ -3,16 +3,22 @@
 import sys
 input =sys.stdin.readline
 
-# 일단 완탐은 
+# 일단 완탐은 -> 시간 괜찮나
 n = int(input())
 arr = list(map(int, input().split()))
 result = []
 
-for i in range(1, max(arr)+1):
-    tmp = 0
-    for ele in arr:
-        tmp += abs((i-ele))
-    result.append((tmp,i))
-result.sort()  
-print(result[0][1])    
-        
+# for i in range(1, max(arr)+1):
+#     tmp = 0
+#     for ele in arr:
+#         tmp += abs((i-ele))
+#     result.append((tmp,i))
+# result.sort()  
+# print(result[0][1])    
+
+# 이 문제는 중간값으로 계산하면 되므로
+arr.sort()
+
+# 중간값
+median_index = arr[(n-1)//2]
+print(median_index) # median_index = arr[(n-1)//2]
