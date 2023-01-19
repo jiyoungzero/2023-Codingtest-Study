145f29ff5fa312630a81ec035f3899d0d559fc69
이지영 <wldudwldhd@naver.com>
2023-01-20 03:01:13 +0900

    [BOJ]오르막수/230120/실버1/DP/재풀이필수/TRY1

--- jiyoung풀이/boj_11057오르막수.py
+++ jiyoung풀이/boj_11057오르막수.py
@@ -1,23 +1,17 @@
-# 소마 코테 준비용 문제
+# 소마 코테 준비용 문제 -> dp인가봄 -> 맨 뒤에서 부터 규칙 찾아보기!!
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-result = 0
 
-# 시간초과 => 오답
-# if n == 1:
-#     print(10)
-# else:
-#     for i in range(10, 10**n):
-#         i = str(i)
-#         flag = True
-#         for j in range(len(i)-1):
-#             if int(i[j]) > int(i[j+1]):
-#                 flag=False
-#                 break
-#         if flag:
-#             result += 1
-#     print(10+result)  
+dp = [1]*10
+
+for i in range(1,n):
+    for j in range(1,10):
+        dp[j] += dp[j-1]
+        
+        
+   
+print((sum(dp[0:]))%10007)
         
