3e824b503ae4490ccf889f210a0b2b08f56d5167
이지영 <wldudwldhd@naver.com>
2023-01-20 01:59:15 +0900

    [BOJ]오르막수/230120/실버1/시간초과오답/try1

---
+++ jiyoung풀이/boj_11057오르막수.py
@@ -0,0 +1,23 @@
+# 소마 코테 준비용 문제
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+result = 0
+
+# 시간초과 => 오답
+# if n == 1:
+#     print(10)
+# else:
+#     for i in range(10, 10**n):
+#         i = str(i)
+#         flag = True
+#         for j in range(len(i)-1):
+#             if int(i[j]) > int(i[j+1]):
+#                 flag=False
+#                 break
+#         if flag:
+#             result += 1
+#     print(10+result)  
+        
