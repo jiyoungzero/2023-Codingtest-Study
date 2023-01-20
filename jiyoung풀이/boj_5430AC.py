2003866469876d21a1a09f74ae9ce409746acf7a
이지영 <wldudwldhd@naver.com>
2023-01-20 19:31:44 +0900

    [BOJ]AC/230120/재풀이필요/try1

--- jiyoung풀이/boj_5430AC.py
+++ jiyoung풀이/boj_5430AC.py
@@ -1,32 +1,74 @@
-# 
+# 큐 
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 test_case = int(input())
 
+
+# 시간초과      
+# 구현 + 뒤집기 아이디어 추가 R 이 홀수 일때만 
+
+# for _ in range(test_case):
+#     flag = True
+#     p = input().rstrip()
+#     n = int(input())
+#     case = input()
+#     tmp = case[1:-2]
+#     tmp = tmp.split(",")
+#     arr = []
+#     if len(tmp) > 2:
+#         for ele in tmp:
+#             arr.append(int(ele))
+
+#     cnt = 0 # 뒤집기 횟수
+#     for command in p:
+#         if command == "R":
+#             cnt += 1
+        
+#         elif command == "D":
+#             if cnt%2 != 0:
+#                 arr = arr[::-1]
+#                 cnt = 0
+#             if arr:
+#                 del arr[0]                
+#             elif not arr:
+#                 print("error")
+#                 flag = False
+#                 break
+#     if flag:print(arr)
+            
+# deque 이용하기 
+
 for _ in range(test_case):
-    flag = True
     p = input().rstrip()
     n = int(input())
-    case = input()
-    tmp = case[1:-2]
-    tmp = tmp.split(",")
-    arr = []
-    if len(tmp) > 2:
-        for ele in tmp:
-            arr.append(int(ele))
-    print(arr)
+    arr = input().rstrip()[1:-1].split(',')
     
+    queue = deque(arr)
+    flag = True
     
-    
-    for command in p:
-        if command == "R":
-            arr = arr[::-1]
-        elif command == "D":
-            if not arr:
-                print("error")
+    # 예외처리
+    if n == 0:
+        queue = []
+
+    cnt = 0
+    for i in p:
+        if i == "R": cnt += 1
+        elif i == "D":
+            if len(queue) < 1:
                 flag = False
-            else:del arr[0]
-    if flag:print(arr)
-                
-            
+                print("error")
+                break
+            else:
+                if cnt%2 != 0:
+                    queue.pop() # 뒤집고 하나 빼기
+                else:
+                    queue.popleft()
+                    
+    if flag:
+        if cnt%2 == 1:
+            queue.reverse()
+            print("["+",".join(queue)+"]")
+        else:
+            print("["+",".join(queue)+"]")
