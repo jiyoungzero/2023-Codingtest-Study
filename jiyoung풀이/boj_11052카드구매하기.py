2fb55c9d4192918704635515c8fc4796a5d6f6d4
이지영 <wldudwldhd@naver.com>
2023-01-23 00:16:10 +0900

    [BOJ]카드구매하기/실버1/230123/30분/수정함

--- jiyoung풀이/boj_11052카드구매하기.py
+++ jiyoung풀이/boj_11052카드구매하기.py
@@ -8,9 +8,8 @@ dp = [0]*(n+1)
 
 dp[1] = cards[1]
 for i in range(2, n+1):
-    tmp = []
     for j in range(i+1):
-        if j <= n:
-            tmp.append(dp[i-j]+cards[j])
-    dp[i]=max(tmp)
+        # if j <= n: 이거 없어도 되는 구나나
+            dp[i]= max(dp[i],dp[i-j]+cards[j])
+
 print(dp[n])
