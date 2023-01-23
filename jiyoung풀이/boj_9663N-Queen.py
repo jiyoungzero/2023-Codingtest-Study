6db8fcc491ca7ae129a443ecde133ca4debf1429
이지영 <wldudwldhd@naver.com>
2023-01-23 23:12:30 +0900

    [BOJ]N-Queen/골드4/230123/recurserror/모범답안/재시도필요/try2

--- jiyoung풀이/boj_9663N-Queen.py
+++ jiyoung풀이/boj_9663N-Queen.py
@@ -1,33 +1,28 @@
 # 구현?
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 n = int(input())
-arr = [[0]*n for _ in range(n)] # 가능 0, 이미 차있음 1
+row = [0] * n
 result = 0
 
-def put(pos,num):
-    row, col = pos
-    for i in range(n): # 행, 열
-        arr[row][i] = num
-        arr[i][col] = num
-    for i in range(n): # 대각선 
-        for j in range(n):
-            if i-j == 0 or i+j == (n-1):
-                arr[i][j] = num
+def flag(x):
+    # 행 row[i], 열 j  --> abs(row[x] - row[i]) == abs(x - i):
+    for i in range(x):
+        if (row[i] == row[x]) or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
 
 def nqueen(x):
     global result
     if x == n:
         result += 1
-    for i in range(n):
+        return
+    else:
         for j in range(n):
-            if arr[i][j] == 0:
-                put((i, j), 1) # 채우고
-                x += 1
-                nqueen(x)
-                put((i,j), 0) # 다시 원래대로
+            row[x] = j
+            if flag(x):
+                nqueen(x+1)
                 
 nqueen(0)
 print(result)
