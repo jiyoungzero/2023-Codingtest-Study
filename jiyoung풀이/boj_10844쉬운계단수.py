bfd0089f71795b1c2f528ea2cc970c50962b089f
이지영 <wldudwldhd@naver.com>
2023-01-26 00:07:35 +0900

    [BOJ]쉬운계단수/실버1/230126/재풀이필요/try1

--- jiyoung풀이/boj_10844쉬운계단수.py
+++ jiyoung풀이/boj_10844쉬운계단수.py
@@ -6,20 +6,16 @@ input =sys.stdin.readline
 n = int(input())
 dp = [[0]*(10) for _ in range(n+1)]
 
-for i in range(10):
+for i in range(1,10):
     dp[1][i] = 1
-for i in range(n+1):
-    dp[i][0] = i
 
-
-for i in range(2,n+1):
-    for j in range(1,9):
-        if i == 3 and j == 1:
-            dp[i][j] = 3
+# 맨 뒤에서 부터 생각하자!!
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1]
         else:
-            dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
-            
-for i in range(2,n+1):
-    dp[i][9] = dp[i][8] // 2
-
-print(sum(dp[-1][1:]))
+            dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1]
+print(sum(dp[-1])%1000000000)
