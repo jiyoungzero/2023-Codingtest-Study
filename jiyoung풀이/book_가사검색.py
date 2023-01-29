c5cd27e619ed7cadb3ad69adfbe4ca5644b1f3fc
이지영 <wldudwldhd@naver.com>
2023-01-29 23:31:01 +0900

    [이코테]가사검색/230129/이진탐색/재풀이필수/try2

--- jiyoung풀이/book_가사검색.py
+++ jiyoung풀이/book_가사검색.py
@@ -24,3 +24,35 @@ def solution(words, queries):
     
     return answer
 
+# 이진탐색 코드 !!!
+from bisect import bisect_left, bisect_right
+# 단어 개수별로 리스트 만들고 정렬
+# for?? 쿼리일땐, 길이가 5인 단어만 검사
+# 검사할때, fro로 시작되는 첫 단어와 마지막 단어의 인덱스의 차 + 1 하면 됨
+
+def count_by_value(a, left, right):
+    return  bisect_right(a, right)-bisect_left(a, left)
+
+def solution(words, queries):
+    answer = []
+    words_lst = [[] for _ in range(10001)]
+    reversed_lst = [[] for _ in range(10001)]
+    for w in words:
+        words_lst[len(w)].append(w)
+        reversed_lst[len(w)].append(w[::-1])
+
+    # 정렬
+    for i in range(10001):
+        words_lst[i].sort()
+        reversed_lst[i].sort()
+
+    for q in queries:
+        tmp = 0
+        if q[0] == "?":
+            tmp = count_by_value(reversed_lst[len(q)], q[::-1].replace("?", "a"), q[::-1].replace("?","z"))
+        else:
+            tmp = count_by_value(words_lst[len(q)], q.replace("?","a"), q.replace("?","z"))
+
+        answer.append(tmp)
+
+    return answer
