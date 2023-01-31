0cbb557d152e3cabcd21a2b71b1da537402247bd
이지영 <wldudwldhd@naver.com>
2023-02-01 00:34:35 +0900

    [이코테]숨바꼭질/모법답안/230201/try1

--- jiyoung풀이/book_숨바꼭질.py
+++ jiyoung풀이/book_숨바꼭질.py
@@ -30,4 +30,45 @@ while q:
 result = max(distance)
 print(distance.index(result), result, distance.count(result))     
 
-            
+# 정답 코드
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = 1 # 시작 노드 1번 헛간
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1) 
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+    
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+dijkstra(start)
+
+max_node = 0 # 동빈이가 숨을 헛간 노드 번호
+max_distance = 0 
+result = [] # 동일한 최단 거리를 가지는 노드들의 리스트
+
+for i in range(1,n+1):
+    if max_distance < distance[i]:
+        max_distance = distance[i]
+        max_node = i
+        result = [max_node]
+    elif max_distance == distance[i]:
+        result.append(i)
+print(max_node, max_distance, len(result))
