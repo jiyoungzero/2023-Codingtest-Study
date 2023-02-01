2345b592fc98d96d01f9e90f695e1d938800850f
이지영 <wldudwldhd@naver.com>
2023-02-02 00:22:36 +0900

    [BOJ]숨바꼭질/실버1/230202/BFS로해결/try1

--- jiyoung풀이/boj_1697숨바꼭질.py
+++ jiyoung풀이/boj_1697숨바꼭질.py
@@ -3,33 +3,22 @@
 import sys
 input = sys.stdin.readline
 import heapq
+from collections import deque
 INF = int(1e9)
 
 n, k = map(int, input().split())
 start = n
 distance = [INF] * (100001)
-graph = [ [] for i in range(100001)]
 
-for i in range(1,100000): # 간선 정보 입력
-    if 2*i<100001:
-        graph[i].append((2*i,1))    
-    graph[i].append((i+1,1))
-    graph[i].append((i-1,1))
+distance[start] = 0
+q = deque([start])
+while q:
+    now = q.popleft()
+    if now == k:break
 
-        
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start)) # 시작 노드일때는 시간 0으로 초기화
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if now == k:return distance
-        if dist > distance[now]:
-            continue
-        for i in graph[now]:
-            if i[1]+dist < distance[i[0]]:
-                distance[i[0]] = i[1]+dist
-                heapq.heappush(q, (i[1]+dist, i[0]))
+    for i in (now-1, now+1, 2*now):
+        if 0<=i<100001 and distance[i] == INF:
+            distance[i] = distance[now] + 1
+            q.append(i)
      
-dijkstra(start)
 print(distance[k])
