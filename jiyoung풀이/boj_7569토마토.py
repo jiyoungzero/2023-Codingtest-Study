b9c2723ebe0575bb8ef453d80541a561bcacd6e9
이지영 <wldudwldhd@naver.com>
2023-02-05 02:21:29 +0900

    [BOJ]토마토/골드5/230205/실패/다시

---
+++ jiyoung풀이/boj_7569토마토.py
@@ -0,0 +1,55 @@
+# dfs?
+import sys
+input= sys.stdin.readline
+from collections import deque
+
+m, n, h= map(int, input().split()) # m: col, n: row, h: height
+arr = []
+for _ in range(h):
+    tmp = []
+    for _ in range(n):
+        tmp.append(list(map(int, input().split())))
+    arr.append(tmp)
+
+direction = [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
+time = 0
+
+# 1:익은 토마토, 0:안익은 토마토, -1:빈칸, 방문한 거: 2
+def in_range(x, y, z):
+    return 0<=x<n and 0<=y<m and 0<=z<h
+
+
+def dfs(x, y, z):
+    if x < 0 or x<=n or y<0 or y<=m or z<0 or z>=0:
+        return False
+    
+    if arr[z][x][y] != 2:
+        arr[z][x][y] = 2 # 방문처리
+        dfs(x, y, z+1)
+        dfs(x, y, z-1)
+        dfs(x+1, y, z)
+        dfs(x-1, y, z)
+        dfs(x, y+1, z)
+        dfs(x, y-1, z)
+        return True
+            
+
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if arr[i][j][k] == 1:
+                if dfs(j,k,i) == True:
+                    time += 1
+                    
+flag = True
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if arr[i][j][k] == 0:
+                flag = False
+                break
+if flag:
+    print(time)
+else:
+    print(-1)
+    
