4951acc14866cfb744ded0f6479b81450aac2c7e
이지영 <wldudwldhd@naver.com>
2023-02-05 02:44:14 +0900

    [BOJ]토마토/230205/골드5/성공/다시해보기/try1

--- jiyoung풀이/boj_7569토마토.py
+++ jiyoung풀이/boj_7569토마토.py
@@ -1,4 +1,4 @@
-# dfs?
+# bfs, dfs로도 할 수 있남
 import sys
 input= sys.stdin.readline
 from collections import deque
@@ -13,43 +13,87 @@ for _ in range(h):
 
 direction = [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
 time = 0
+q = deque()
 
-# 1:익은 토마토, 0:안익은 토마토, -1:빈칸, 방문한 거: 2
+# 토마토 큐에 넣기
+for z in range(h):
+    for x in range(n):
+        for y in range(m):
+            if arr[z][x][y] == 1:
+                q.append((x, y, z)) 
+
 def in_range(x, y, z):
     return 0<=x<n and 0<=y<m and 0<=z<h
 
-
-def dfs(x, y, z):
-    if x < 0 or x<=n or y<0 or y<=m or z<0 or z>=0:
-        return False
-    
-    if arr[z][x][y] != 2:
-        arr[z][x][y] = 2 # 방문처리
-        dfs(x, y, z+1)
-        dfs(x, y, z-1)
-        dfs(x+1, y, z)
-        dfs(x-1, y, z)
-        dfs(x, y+1, z)
-        dfs(x, y-1, z)
-        return True
-            
-
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if arr[i][j][k] == 1:
-                if dfs(j,k,i) == True:
-                    time += 1
-                    
+def bfs():
+    while q:
+        x, y, z = q.popleft()
+        for k in direction:
+            nx, ny, nz = x+k[1], y+k[2], z+k[0]
+            if in_range(nx, ny, nz) and arr[nz][nx][ny] == 0:
+                arr[nz][nx][ny] = arr[z][x][y] + 1
+                q.append((nx, ny,nz))
+bfs()
 flag = True
 for i in range(h):
     for j in range(n):
+        tmp = max(arr[i][j])
         for k in range(m):
             if arr[i][j][k] == 0:
                 flag = False
                 break
-if flag:
-    print(time)
-else:
-    print(-1)
+        if time < tmp:
+            time = tmp
+
+if flag:print(time-1)
+else:print(-1)        
+                
+
+
+
+
+
+
+
+
+
+## 실패 코드 ##
+# 1:익은 토마토, 0:안익은 토마토, -1:빈칸, 방문한 거: 2
+# def in_range(x, y, z):
+#     return 0<=x<n and 0<=y<m and 0<=z<h
+
+
+# def dfs(x, y, z):
+#     if x < 0 or x<=n or y<0 or y<=m or z<0 or z>=0:
+#         return False
+    
+#     if arr[z][x][y] != 2:
+#         arr[z][x][y] = 2 # 방문처리
+#         dfs(x, y, z+1)
+#         dfs(x, y, z-1)
+#         dfs(x+1, y, z)
+#         dfs(x-1, y, z)
+#         dfs(x, y+1, z)
+#         dfs(x, y-1, z)
+#         return True
+            
+
+# for i in range(h):
+#     for j in range(n):
+#         for k in range(m):
+#             if arr[i][j][k] == 1:
+#                 if dfs(j,k,i) == True:
+#                     time += 1
+                    
+# flag = True
+# for i in range(h):
+#     for j in range(n):
+#         for k in range(m):
+#             if arr[i][j][k] == 0:
+#                 flag = False
+#                 break
+# if flag:
+#     print(time)
+# else:
+#     print(-1)
     
