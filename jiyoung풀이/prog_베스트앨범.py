e0538877fdcc8407986c1ad639d57a522b1d42d0
이지영 <wldudwldhd@naver.com>
2023-02-11 01:54:33 +0900

    [PROG]베스트앨범/230211/딕셔너리,defaultdict(list)/try2

--- jiyoung풀이/prog_베스트앨범.py
+++ jiyoung풀이/prog_베스트앨범.py
@@ -27,3 +27,34 @@
 #             answer.append(best[0])
 
 #     return answer
+
+# 정답
+from collections import defaultdict
+
+def solution(genres, plays):
+    answer = []
+    playlist = []
+    forplays = defaultdict(list)
+
+    # 각 장르별 총 재생 수 구하기
+    for g, p in zip(genres, plays):
+        forplays[g].append(p)
+
+
+    for g_idx, g in enumerate(genres):
+        for p_idx, p in enumerate(plays):
+            if g_idx == p_idx:
+                playlist.append((g_idx, g, p, sum(forplays[g]))) # idx, 장르, 개인플레이수, 장르플레이수
+
+    playlist.sort(key=lambda x:(-x[3], -x[2], x[0]))
+
+    cnt = dict() # 두곡까지만 수록
+    for i in set(genres):cnt[i] = 0
+
+    for ele in playlist:
+        if cnt[ele[1]] >= 2:continue
+        answer.append(ele[0])
+        cnt[ele[1]] += 1
+
+
+    return answer
