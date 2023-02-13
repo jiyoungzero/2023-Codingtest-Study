cd79eec39b8e8ea38da1b67110afdf9af8a6d630
이지영 <wldudwldhd@naver.com>
2023-02-13 18:54:35 +0900

    [BOJ]케빈베이컨의6단계법칙/230212/성공/플로이드워셜/try1

--- jiyoung풀이/boj_1389케빈베이컨의6단계법칙.py
+++ jiyoung풀이/boj_1389케빈베이컨의6단계법칙.py
@@ -2,9 +2,30 @@
 
 import sys
 input =sys.stdin.readline
+INF = int(1e9)
 
 
 n, m = map(int, input().split())
-graph = []
+graph = [[INF]*(n+1) for _ in range(n+1)]
 for _ in range(m):
-    graph.append()
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+    
+# 자기 자신한테 가는 거 0
+for i in range(n+1):
+    graph[i][i] = 0
+    
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+result = INF
+result_idx = INF
+for i in range(n, 0, -1):
+    tmp = sum(graph[i][1:])
+    result = min(result, tmp)
+    if result == tmp:
+        result_idx = i
+print(result_idx)
