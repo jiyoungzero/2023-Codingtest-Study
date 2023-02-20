9c37d2b02a9803bbedab4ab8b213ac8d8e0ad999
이지영 <wldudwldhd@naver.com>
2023-02-21 00:12:14 +0900

    [BOJ]소년점프/230221/골드4/성공/TRY2

--- jiyoung풀이/boj_16469소년점프.py
+++ jiyoung풀이/boj_16469소년점프.py
@@ -20,26 +20,27 @@ for _ in range(3):
 dxs, dys = [0,0,1,-1],[1,-1,0,0]
 bad_history = []
 
-def bfs(x, y):
+def bfs(start_x, start_y):
     queue = deque()
-    queue.append((x,y))
+    queue.append((start_x,start_y))
     tmp_graph = [g[:] for g in graph]
-    tmp_graph[x][y] = 0
+
     while queue:
         x, y = queue.popleft()
         for k in range(4):
             nx, ny = x+dxs[k], y+dys[k]
             if 0>nx or r<=nx or 0>ny or c<=ny : continue
-            if graph[nx][ny] == 1:continue
             if tmp_graph[nx][ny] == 0: # 방문한적없고, 벽이 없으면 
                 tmp_graph[nx][ny] = tmp_graph[x][y] + 1 # 시간 기록
                 queue.append((nx, ny))
-    tmp_graph[x][y] = 0   # 가만히 있는 경우 
+    
     
     # 방문안한곳은 나중에 연산 쉽게 하기 위해서 -1
     for i in range(r):
         for j in range(c):
             if tmp_graph[i][j] == 0:tmp_graph[i][j] = -1
+            
+    tmp_graph[start_x][start_y] = 0   # 가만히 있는 경우 
     return tmp_graph
     
 # 악당들의 총 시간 합치기
@@ -55,6 +56,7 @@ for i in range(r):
             result.append(max(bad_history[0][i][j],bad_history[1][i][j],bad_history[2][i][j]))
 
 # result에 아무것도 안 담기면 같은 자리에 동일하게 도달할 수 있는 경우가 없음 -> -1
+print(result, bad_history)
 if len(result) == 0:print(-1)
 else:
     result.sort()
