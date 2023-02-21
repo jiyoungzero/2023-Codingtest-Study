0d982659aab7d698775f47238ded67462a27cb29
이지영 <wldudwldhd@naver.com>
2023-02-21 18:08:33 +0900

    [BOJ]적록색약/230221/골드5/성공/TRY2

--- jiyoung풀이/boj_10026적록색약.py
+++ jiyoung풀이/boj_10026적록색약.py
@@ -2,7 +2,7 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
+sys.setrecursionlimit(100000)
 n = int(input())
 normal = []
 dxs, dys = [0,0,1,-1],[1,-1,0,0]
@@ -14,45 +14,33 @@ for i in range(n):
         if not_normal[i][j] == "R":
             not_normal[i][j] = "G"
 
-def dfs(x, y):
-    if x < 0 or x>=n or y<0 or y>=n:
-        return False
-    if not visited[x][y]:
-        visited[x][y] = True
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        return True
-    return False
-    
-def bfs(start_x, start_y, graph): 
-    global cnt, visited
-    que = deque()
-    que.append((start_x, start_y))
-    visited[start_x][start_y] = True # 방문 처리
-    
-    while que:
-        x, y = que.popleft()
-        for k in range(4):
-            nx, ny = x+dxs[k], y + dys[k]
-            if 0>nx or n <= nx or 0>ny or n<=ny:continue
-            if visited[nx][ny]: continue
-            
-            if graph[nx][ny] != graph[x][y]: # 색이 다르면
-                if dfs(nx, ny):
-                    cnt[graph[nx][ny]] += 1
-                
-            if graph[nx][ny] == graph[x][y]: # 색이 같으면 
-                que.append((nx,ny))
+def dfs(x, y,graph):
+    cur = graph[x][y]
+    visited[x][y] = True
+    for k in range(4):
+        nx, ny = x+dxs[k], y+dys[k]
+        if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+            if cur == graph[nx][ny] : # 같은 색의 뭉탱이 
                 visited[nx][ny] = True
+                dfs(nx, ny, graph)
+            
+visited = [[False]*n for _ in range(n)]
+normal_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            dfs(i,j,normal)
+            normal_cnt += 1
 
-for arr in [normal, not_normal]:
-    cnt = {"R":1,"G":1,"B":1} 
-    visited = [[False]*n for _ in range(n)]
-    bfs(0,0,arr)
-    print(cnt)
-    if arr == not_normal:
-        print(sum(cnt.values())-1, end=" ")
-    else:print(sum(cnt.values()), end=" ")
+visited = [[False]*n for _ in range(n)]
+not_normal_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            dfs(i,j,not_normal)
+            not_normal_cnt += 1
+            
+            
+print(normal_cnt, not_normal_cnt)
+        
             
