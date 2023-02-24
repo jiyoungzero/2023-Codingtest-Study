977e6be526aa58a4bd6d4657abf412f0f758268e
이지영 <wldudwldhd@naver.com>
2023-02-24 23:17:50 +0900

    [BOJ]숨바꼭질/실버1/230224/BFS/TRY2

--- jiyoung풀이/boj_1697숨바꼭질.py
+++ jiyoung풀이/boj_1697숨바꼭질.py
@@ -1,24 +1,36 @@
 # 그래프탐색
 
 import sys
-input = sys.stdin.readline
-import heapq
+input =sys.stdin.readline
 from collections import deque
 INF = int(1e9)
-
 n, k = map(int, input().split())
-start = n
-distance = [INF] * (100001)
+time = [INF] * (100001)
 
-distance[start] = 0
-q = deque([start])
-while q:
-    now = q.popleft()
-    if now == k:break
+def bfs(start_x):
+    que = deque([start_x])
+    time[start_x] = 0
+    while que:
+        x = que.popleft()
+        if x == k:break
+        
+        # 아래는 시간초과 뜸
+        # if 0<=(x-1)<100001 and time[x-1] == INF  :
+        #     time[x-1] = time[x] + 1
+        #     que.append(x-1)
+        # if   0<=(x+1)<100001 and time[x+1] == INF:
+        #     time[x-1] = time[x] + 1
+        #     que.append(x+1)
+        # if 0<=(x*2)<100001  and time[x*2] == INF:
+        #     time[x*2] = time[x] + 1
+        #     que.append(x*2)
+        
+        # 중요 > 인덱스 검사를 먼저 해놓기 그래야 인덱스 에러가 안남 
+        for i in (x-1, x+1, x*2):
+            if 0<=i<100001 and time[i] == INF:
+                time[i] = time[x] + 1
+                que.append(i)
 
-    for i in (now-1, now+1, 2*now):
-        if 0<=i<100001 and distance[i] == INF:
-            distance[i] = distance[now] + 1
-            q.append(i)
-     
-print(distance[k])
+bfs(n)
+print(time[k])     
+    
