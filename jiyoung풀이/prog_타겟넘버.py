1dd98d5ae2d103474a69deae77babc36b7af643e
이지영 <wldudwldhd@naver.com>
2023-02-28 21:51:15 +0900

    [PROG]타겟넘버/230228/DFS재귀/다시풀기/TRY1

--- jiyoung풀이/prog_타겟넘버.py
+++ jiyoung풀이/prog_타겟넘버.py
@@ -17,3 +17,22 @@ def solution(numbers, target):
     return answer
 
 # dfs -> 하나씩 비교해 가면서 재귀로 풀기 https://velog.io/@timointhebush/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%83%80%EA%B2%9F-%EB%84%98%EB%B2%84-DFS-BFS-Python
+
+
+def solution(numbers, target):
+    answer = dfs(numbers, target, 0)
+
+    return answer
+
+def dfs(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        if sum(numbers) == target:
+            return 1
+        else:return 0
+
+    else:
+        answer += dfs(numbers, target, depth+1) # 더하기로 시작해서 여기로 재귀에 들어가면 +1, -1 모두 겪게 된다. 
+        numbers[depth] *= (-1)
+        answer += dfs(numbers, target, depth+1) # 빼기로 시작해서 여기로 재귀에 들어가면 +1, -1 모두 들어가게 된다. -> 깊이 우선 탐색!!
+    return answer
