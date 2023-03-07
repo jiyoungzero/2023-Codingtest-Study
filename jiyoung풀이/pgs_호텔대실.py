18b40efb17ac9671506a6e0e5d332106459125f5
이지영 <wldudwldhd@naver.com>
2023-03-07 18:41:13 +0900

    [PGS]호텔대실/230307/누적합/다시풀기/TRY2

--- jiyoung풀이/pgs_호텔대실.py
+++ jiyoung풀이/pgs_호텔대실.py
@@ -52,3 +52,41 @@ def solution(book_time):
         afterClean(arr, time[1])
 
     return answer
+
+
+
+## 누적합!! -> 겹치지 않도록 하는 문제에서는 누적합이 좋음 
+def solution(book_time):
+    time_table = [0 for _ in range(60 * 24)]
+    for start, end in book_time:
+        start_minutes = 60 * int(start[:2]) + int(start[3:])
+        end_minutes = 60 * int(end[:2]) + int(end[3:]) + 10
+
+        if end_minutes > 60 * 24 - 1:
+            end_minutes = 60 * 24 - 1
+
+        for i in range(start_minutes, end_minutes):
+            time_table[i] += 1
+    return max(time_table)
+
+
+# 그 외의 풀이 -> 우선순위 큐 이용
+from heapq import heappop, heappush
+
+def solution(book_time):
+    answer = 1
+    book_time_ref = [(int(s[:2]) * 60 + int(s[3:]), int(e[:2]) * 60 + int(e[3:])) for s, e in book_time]
+    book_time_ref.sort()
+
+    heap = []
+    for s, e in book_time_ref:
+        if not heap:
+            heappush(heap,e)
+            continue
+        if heap[0] <= s:
+            heappop(heap)
+        else:
+            answer += 1
+        heappush(heap,e+10)
+
+    return answer
