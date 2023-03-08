8e846bd73e63ea0a3c608551c787cce6d6fe9db9
이지영 <wldudwldhd@naver.com>
2023-03-08 19:01:38 +0900

    [PGS]네트워크/230308/BFS풀이/성공/TRY2

--- jiyoung풀이/pgs_네트워크.py
+++ jiyoung풀이/pgs_네트워크.py
@@ -23,3 +23,31 @@ def bfs(start,visited, computers):
                     que.append(i)
                     ans += 1
     return ans
+
+# bfs는 연결성, que가 끝나면 연결도 끝나는 것 (네트워크)
+from collections import deque 
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+
+    for connect in range(n):
+        if not visited[connect]:
+            bfs(n, connect, visited, computers)
+            answer += 1
+
+    return answer
+
+
+def bfs(n, start,visited, computers):
+    que = deque()
+    visited[start] = True
+    que.append(start)
+    while que:
+        x = que.popleft()
+        visited[x] = True
+        for i in range(n):
+            if i != x and computers[x][i]:
+                if not visited[i]:
+                    que.append(i)
