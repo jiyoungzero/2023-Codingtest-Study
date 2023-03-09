7312fe8f3f218965fdbdd9f2be5722f721b31218
이지영 <wldudwldhd@naver.com>
2023-03-09 18:47:55 +0900

    [PGS]완주하지못한선수/230309/효율성통과/Counter객체/try2

--- jiyoung풀이/pgs_완주하지못한선수.py
+++ jiyoung풀이/pgs_완주하지못한선수.py
@@ -1,2 +1,37 @@
 # 해시 - map
 
+# 테스트는 통과했지만 시간초과 (효율성 실패)
+def solution(participant, completion):
+    answer = ''
+    
+    for com in completion:
+        if com in participant:
+            participant.remove(com)
+            
+    return participant[0]
+
+
+# 통과한 풀이
+def solution(participant, completion):
+    answer = ''
+
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            return answer 
+
+    return participant[-1]
+
+
+# Colletions의 Counter 객체 -> 딕셔너리 형태로 key값이 몇번 반복되는지를 알려주는 라이브러리
+
+import collections
+
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    print(answer)
+    return list(answer.keys())[0]
