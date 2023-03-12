c6f54e94bd1d341cdf9b43d177e5955a696f193c
이지영 <wldudwldhd@naver.com>
2023-03-12 23:51:09 +0900

    [PGS]체육복/230312/그리디/실패/TRY2

--- jiyoung풀이/pgs_체육복.py
+++ jiyoung풀이/pgs_체육복.py
@@ -1,23 +1,25 @@
 # 그리디 
 
-# 시간초과
+# 84점 -> 실패
 def solution(n, lost, reserve):
     answer = n-len(lost)
-
+    s_lost = set()
+    s_reserve =set(reserve)
+    
     for ele in lost:
-        if ele == 1 or ele == n:
-            if 2 in reserve:
-                answer += 1 
-                reserve.remove(2)
-            elif n in reserve:
-                answer += 1
-                reserve.remove(n-1)
-        elif (ele-1) in reserve :
-                answer += 1
-                reserve.remove(ele-1)
-        elif (ele+1) in reserve:
-                answer += 1
-                reserve.remove(ele+1)
+        a, b = ele-1, ele+1
+        if 1<=a<=n:
+            s_lost.add(a)
+        if 1<=b<=n:
+            s_lost.add(b)
+    print(s_lost, s_reserve)      
+    
+    
+    plus = len(s_lost & s_reserve)
+    
+    if len(s_lost) <= plus:
+        plus = len(lost)
 
+    answer += plus
 
     return answer
