c7e4d7a64af51bda4f44973fb53b3118255cbcd4
이지영 <wldudwldhd@naver.com>
2023-03-15 00:54:58 +0900

    [PGS]가장큰수/230314/다시보기/TRY2

--- jiyoung풀이/pgs_가장큰수.py
+++ jiyoung풀이/pgs_가장큰수.py
@@ -1,17 +1,12 @@
 # 정렬
 
+# 다시 보기
 def solution(numbers):
     answer = ''
-    new_numbers = []
-    
-    for num in numbers:
-        new_numbers.append(list(str(num)))
-        
-    new_numbers.sort(reverse=True)
-    
-    for num in new_numbers:
-        print(num)
-        for i in range(len(num)):
-            answer += num[i]
-    
-    return answer
+    new_numbers = list(map(str, numbers))
+    new_numbers.sort(reverse=True, key = lambda x:x*3)
+
+    # print("".join(new_numbers))
+
+
+    return str(int("".join(new_numbers)))
