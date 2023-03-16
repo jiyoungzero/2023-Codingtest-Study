71e267da9c5113b8d64e08cc3002fdc759568396
이지영 <wldudwldhd@naver.com>
2023-03-16 17:43:16 +0900

    [BOJ]균형잡힌세상/실버4/스택_성공/TRY2

--- jiyoung풀이/boj_4949균형잡힌세상.py
+++ jiyoung풀이/boj_4949균형잡힌세상.py
@@ -2,27 +2,31 @@
 # import sys
 # input = sys.stdin.readline
 
-input_lst = []
+
 
 while 1:
     string = input()
     if string == ".":
         break
-    else:input_lst.append(string)
-    
-for string in input_lst:
     stack = []
     flag = True
     for s in string:
         if s == "(" or s=="[":
             stack.append(s)
-        elif s==")" or s == "]":
+        elif s==")":
             if len(stack) > 0:
-                if stack[-1] == "(" and s == ")":stack.pop()
-                elif stack[-1] == "[" and s == "]":stack.pop()
-                else:flag = False
+                if stack[-1] == "(" :stack.pop()
+                else:
+                    flag = False
+                    break
+        elif s=="]":
+            if len(stack) > 0:
+                if stack[-1] == "[" :stack.pop()
+                else:
+                    flag = False
+                    break
         else:continue
-    if len(stack) > 0 or flag == False:
-        print("no")
-    else:print("yes")
+    if flag and len(stack)==0:
+        print("yes")
+    else:print("no")
 
