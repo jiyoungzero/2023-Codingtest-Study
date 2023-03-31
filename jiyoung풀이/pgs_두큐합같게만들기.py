d533e4af981d77dccc0fa41ceb72da19663f0f94
이지영 <wldudwldhd@naver.com>
2023-04-01 01:54:15 +0900

    [PGS]두큐합같게만들기/230401/greedy/TRY2

--- jiyoung풀이/pgs_두큐합같게만들기.py
+++ jiyoung풀이/pgs_두큐합같게만들기.py
@@ -2,29 +2,35 @@
 
 # 이거 처음에 감 못잡음 (0/3)
 
-# 백트래킹?
-from copy import deepcopy
-answer = int(1e9)
-def solution(que1, que2):
-    # answer = -2
-    global answer
-    target = sum(que1) + sum(que2) / 2
-    answer = backtracking(que1, que2, target,0)
+# 그리디넹
+from collections import deque
+def solution(queue1, queue2):
+    que1 = deque(queue1)
+    que2 = deque(queue2)
+    sum1 = sum(que1)
+    sum2 = sum(que2)
+    answer = 0
+    max_cnt = len(que1) * 4
+    
+    if (sum1 + sum2)%2 != 0:return -1
+    
+    while 1:
+        if sum1 > sum2:
+            tmp = que1.popleft()
+            que2.append(tmp)
+            sum1 -= tmp
+            sum2 += tmp
+            answer += 1
+        elif sum1 < sum2:
+            tmp = que2.popleft()
+            que1.append(tmp)
+            sum1 += tmp
+            sum2 -= tmp
+            answer += 1
+        else:break
+        
+        if answer == max_cnt:
+            answer = -1
+            break
     
     return answer
-
-def backtracking(q1, q2, target, cnt):
-    global answer
-    if sum(q1) == target:
-        answer = min(answer, cnt)
-        print("ss")
-        return 
-    else:
-        tmp1 = deepcopy(q1)
-        tmp2 = deepcopy(q2)
-        if sum(tmp1) > sum(tmp2):
-            value = tmp1.popleft()
-            tmp2.append(value)
-            backtracking(tmp1, tmp2, target, cnt+1)
-            tmp1.append(value)
-            tmp2.pop()
