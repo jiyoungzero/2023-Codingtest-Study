a5724a33c89b0f029a79af3771566b649fdb9ab6
이지영 <wldudwldhd@naver.com>
2023-04-04 18:09:45 +0900

    [BOJ]수들의합2/230404/실버4/투포인터/TRY2

--- jiyoung풀이/boj_2003수들의합2.py
+++ jiyoung풀이/boj_2003수들의합2.py
@@ -6,14 +6,17 @@ input =sys.stdin.readline
 n, m  = map(int, input().split())
 arr = list(map(int, input().split()))
 answer = 0
-arr.sort() # 1 1 1 2 2 2 3 3 4 5
-print(arr)
+start, end = 0, 0
+tmp_sum = 0
 
-for i in range(n-1):
-    for j in range(i+1, n+1):
-        target = sum(arr[i:j])
-        if target > m:
-            break
-        elif target == m:
-            answer += 1
+for start in range(n):
+    while tmp_sum < m and end < n:
+        tmp_sum += arr[end]
+        end += 1
+    if tmp_sum == m:
+        answer += 1
+    tmp_sum -= arr[start]
+    
 print(answer)
+        
+        
