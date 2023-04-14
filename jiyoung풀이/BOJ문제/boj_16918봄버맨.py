c4303d9d9f0cb2b7634165efac4fd4da2d6b8a25
이지영 <wldudwldhd@naver.com>
2023-04-15 01:13:40 +0900

    [BOJ]붐버맨/실버1/230414/구현,그래프탐색/TRY2

--- jiyoung풀이/BOJ문제/boj_16918봄버맨.py
+++ jiyoung풀이/BOJ문제/boj_16918봄버맨.py
@@ -2,43 +2,43 @@ import sys
 input =sys.stdin.readline
 from collections import deque
 
-r, c, n = map(int, input().split()) # 행, 열, 초
-arr = [list(map(str, input())) for _ in range(r)]
-dx, dy = [0,0,1,-1],[1,-1,0,0]
-# 초기(time%3 == 0) -> 
-# 변하는 거 없음(time%3 == 1) -> 
-# 모든 곳에 폭탄 설치(time%3 == 2) -> 
-# 초기 폭탄 + 상하좌우 . 전환 (time%3 == 0)
-q = deque()
-time = 0
-
-for i in range(r):
-    for j in range(c):
-        if arr[i][j] == "O":
-            q.append((i,j,0)) # x좌표, y좌표, 시간
-
-while time != n:
-    # 1. 초기 : 변화 없음
-    time += 1
-    # 2. 모든 곳에 폭탄 설치
-    for i in range(r):
-        for j in range(c):
-            if arr[i][j] == '.':
-                arr[i][j] = 'O'
-    time += 1
-    # 3. 3초된 폭탄은 터짐(상하좌우 포함)
-    
-    
-    x,y,t = q.popleft()
-    if t == 3:
-        arr[x][y] = '.'
-        for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-            if 0<=nx<r and 0<=ny<c:
-                arr[nx][ny] = '.'
-    q.append()
-                    
-            
-    time += 1
+r, c, n = map(int, input().split())
+arr = [list(map(str, input().rstrip())) for _ in range(r)]
+dx, dy = [0,0,1,-1], [1,-1,0,0]
+if n <= 1:
+    for lst in arr:print("".join(lst))
+else:
+    n -= 1
+    while n:
+        # 1단계 : 폭탄 설치 
+        bomb = deque()
+        # 2단계 : 폭탄 위치 저장 
+        for i in range(r):
+            for j in range(c):
+                if arr[i][j] == "O":
+                    bomb.append((i,j))
         
+        # 3단계 : 모든 위치에 폭탄 설치
+        for i in range(r):
+            for j in range(c):
+                if arr[i][j] == ".":
+                    arr[i][j] = "O"
+        n -=1 
+        if n == 0:break
+        # 4단계 : 폭발
+        n-=1
+        while bomb:
+            x, y = bomb.popleft()
+            arr[x][y] = '.'
+            for k in range(4):
+                nx, ny = x+dx[k], y+dy[k]
+                if 0<=nx<r and 0<=ny<c:
+                    if arr[nx][ny] == "O":
+                        arr[nx][ny] = "."
+        # n -= 1
+                        
+for lst in arr:
+    print("".join(lst))
+                    
+    
 
