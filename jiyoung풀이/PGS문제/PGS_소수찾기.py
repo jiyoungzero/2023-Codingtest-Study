26c10a562e02cc10a57d944973aa5e77a4e5b201
이지영 <wldudwldhd@naver.com>
2023-04-18 02:03:14 +0900

    [PGS]소수찾기/230417/소수판별/성공/try2

--- jiyoung풀이/PGS문제/PGS_소수찾기.py
+++ jiyoung풀이/PGS문제/PGS_소수찾기.py
@@ -1,13 +1,13 @@
 # 완탐 
 
 from itertools import permutations
-
+import math
 def solution(numbers):
     answer = 0
     num_list = []
     for i in range(len(numbers)):
         num_list.append(numbers[i])
-        
+
     n = len(numbers)
     check = []
     visited = set()
@@ -20,17 +20,13 @@ def solution(numbers):
             if int(tmp) not in visited:
                 if isDecimal(int(tmp)):answer+=1
             visited.add(int(tmp))
-    
+
     return answer
 
 def isDecimal(num):
     if num == 1 or num == 0: return False
     else:
-        for i in range(2, num//2+1):
-            if num != 2 and num %2 == 0:
-                return False
-            if num != 5 and num%5 ==0:
-                return False
-            if num != 7 and num%7 == 0:
+        for i in range(2,int(math.sqrt(num))+1):
+            if num%i == 0:
                 return False
     return True
