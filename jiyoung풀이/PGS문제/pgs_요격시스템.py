3bed3bc7f398fe600444ccb34636f72082ea3ca9
이지영 <wldudwldhd@naver.com>
2023-04-30 00:11:32 +0900

    [PGS]요격시스템/LEVEL2/오답/TRY1

--- jiyoung풀이/PGS문제/pgs_요격시스템.py
+++ jiyoung풀이/PGS문제/pgs_요격시스템.py
@@ -1,7 +1,17 @@
 # level2
 
+# 오답 
 def solution(targets):
     answer = 0
-    
-    
+    targets.sort(key=lambda x:x[0])
+    prev_s, prev_e = targets[0][0], targets[0][1]
+    all = len(targets)
+    for i in range(1, len(targets)):
+        if prev_s < targets[i][0] < prev_e:
+            prev_s = targets[i][0]
+            
+        else:
+            prev_s, prev_e = targets[i][0], targets[i][1]
+            answer += 1
+        
     return answer
