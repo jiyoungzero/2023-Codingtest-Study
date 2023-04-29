1c123e512886258810e676d2f2527e7762cf31dd
이지영 <wldudwldhd@naver.com>
2023-04-30 00:49:42 +0900

    [PGS]요격시스템/LEVEL2/정렬/다시/TRY2

--- jiyoung풀이/PGS문제/pgs_요격시스템.py
+++ jiyoung풀이/PGS문제/pgs_요격시스템.py
@@ -1,17 +1,15 @@
 # level2
 
-# 오답 
+# 성공 -> 정렬 중요 
 def solution(targets):
     answer = 0
-    targets.sort(key=lambda x:x[0])
-    prev_s, prev_e = targets[0][0], targets[0][1]
-    all = len(targets)
-    for i in range(1, len(targets)):
-        if prev_s < targets[i][0] < prev_e:
-            prev_s = targets[i][0]
-            
-        else:
-            prev_s, prev_e = targets[i][0], targets[i][1]
+    targets.sort(key=lambda x:x[1])
+    s, e = 0,0
+    for t in targets:
+        if s < t[0] < e:continue
+        elif e <= t[0]:
             answer += 1
+            s, e = t[0], t[1]
+        
         
     return answer
