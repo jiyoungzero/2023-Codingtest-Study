b19672068e6469b0e6ac48b02978d731eca17e13
이지영 <wldudwldhd@naver.com>
2023-05-14 20:03:51 +0900

    [PGS]테이블해시함수/230514/문제잘보기/TRY1

---
+++ jiyoung풀이/PGS문제/pgs_테이블해시함수.py
@@ -0,0 +1,12 @@
+
+# xor  = ^
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    data.sort(key=lambda x:(x[col-1], -x[0]))
+
+    for i in range(row_begin, row_end+1):
+        tmp = 0
+        for j in data[i-1]:
+            tmp += (j%i)
+        answer ^= tmp
+    return answer
