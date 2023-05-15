a98fb00b68f42dc5bc1d98485745e2e1ff5ccb36
이지영 <wldudwldhd@naver.com>
2023-05-16 01:38:10 +0900

    [PGS]리코쳇로봇/23056/BFS/정답참고함/다시/TRY1

--- jiyoung풀이/PGS문제/pgs_리코쳇로봇.py
+++ jiyoung풀이/PGS문제/pgs_리코쳇로봇.py
@@ -1,16 +1,48 @@
+from collections import deque
 def solution(board):
     answer = 0
     dx, dy = [0,0,1,-1],[1,-1,0,0]
     s_x, s_y = 0,0
     r, c = len(board), len(board[0])
-    
+    flag = False
+    visited = [[0] * c for _ in range(r)]
+    # 시작점 찾기
     for i in range(r):
         for j in range(c):
             if board[i][j] == "R":
                 s_x, s_y = i, j
                 break
-    
-    
-    
-    
-    return answer
+
+    def bfs():
+        que = deque()
+        que.append((s_x, s_y))
+        visited[s_x][s_y] = 1
+
+
+        while que:
+            x, y = que.popleft()
+            if board[x][y] == "G":
+                flag = True
+                return visited[x][y]
+
+            for k in range(4):
+                nx, ny = x, y
+                while True:
+                    nx += dx[k]
+                    ny += dy[k]
+                    if 0<=nx<r and 0<=ny<c and board[nx][ny] == "D":
+                        nx -= dx[k]
+                        ny -= dy[k]
+                        break
+                    elif 0>nx or r<=nx or 0>ny or c<=ny:
+                        nx -= dx[k]
+                        ny -= dy[k]
+                        break
+                if not visited[nx][ny]:
+                    visited[nx][ny] = visited[x][y] + 1
+                    que.append((nx,ny))
+        return -1
+
+    answer = bfs()
+
+    return answer-1 if answer > 0 else -1
