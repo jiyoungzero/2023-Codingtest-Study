1719fdd739ba28ef74014fb6573a214c5cbf243a
이지영 <wldudwldhd@naver.com>
2023-05-21 17:41:29 +0900

    [PGS]광물캐기/230521/그리디/다른풀이/TRY2

--- jiyoung풀이/PGS문제/pgs_광물캐기.py
+++ jiyoung풀이/PGS문제/pgs_광물캐기.py
@@ -61,3 +61,42 @@ def solution(picks, minerals):
                     # print(minerals[j], answer)
             
     return answer
+
+
+# 다른 풀이
+
+def solution(picks, minerals):
+    answer = 0
+    canTake = sum(picks)*5
+    if len(minerals) > canTake:
+        minerals = minerals[:canTake]
+
+    cnt_mineral = [[0,0,0] for _ in range(10)] # 5묶음 안에 있는 세가지 광물의 개수
+    for i in range(len(minerals)):
+        if minerals[i] == "diamond":
+            cnt_mineral[i//5][0] += 1
+        elif minerals[i] == 'iron': 
+            cnt_mineral[i//5][1] += 1
+        else : 
+            cnt_mineral[i//5][2] += 1
+
+    sorted_cnt_min = sorted(cnt_mineral, key=lambda x :(-x[0], -x[1], -x[2]))
+
+    for mineral in sorted_cnt_min:
+        d, i, s = mineral
+        for p in range(3):
+            if p == 0 and picks[p] > 0:
+                picks[p] -= 1
+                answer += (d+i+s)
+                break 
+            elif p == 1 and picks[p] > 0:
+                picks[p] -= 1
+                answer += (d*5+i+s)
+                break
+            elif p == 2 and picks[p] > 0:
+                picks[p] -= 1
+                answer += (d*25+i*5+s)
+                break
+
+
+    return answer
