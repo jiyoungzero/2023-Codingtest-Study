65d6b3efedff365283388702a20c185bce2d3b88
이지영 <wldudwldhd@naver.com>
2023-05-25 16:34:48 +0900

    [PGS]미로탈출/230525/BFS/실패

--- jiyoung풀이/PGS문제/pgs_미로탈출.py
+++ jiyoung풀이/PGS문제/pgs_미로탈출.py
@@ -1,45 +1,37 @@
-# 오답 
+# 오답 -> 26점 
+from collections import deque
 def solution(maps):
     answer = 0
-    r, c = len(maps), len(maps[0])
-    s_x, s_y = 0,0
     dx, dy = [0,0,1,-1],[1,-1,0,0]
-    visited =[ [0] * c for _ in range(r)]
+    r, c = len(maps), len(maps[0])
+    s_x, s_y = 0, 0
+    visited = [[0]*c for _ in range(r)]
     
-    # 출발지점 찾기
+    # 출발 지점 찾기 
     for i in range(r):
         for j in range(c):
             if maps[i][j] == "S":
                 s_x, s_y = i, j
                 break
                 
-                
-    # 도착지점 찾기
-    for i in range(r):
-        for j in range(c):
-            if maps[i][j] == "E":
-                e_x, e_y = i, j
-                break
-    visited[s_x][s_y] = 1
+    def bfs(start, end, time):
+        que = deque()
+        que.append((s_x, s_y, 1))
+        visited[s_x][s_y] = 1
+        
+        while que:
+            x, y,t = que.popleft()
+            for k in range(4):
+                nx, ny = x +dx[k], y + dy[k]
+                if 0<=nx<r and 0<= ny<c:
+                    if not visited[nx][ny]:
+                        if maps[nx][ny] == "L" or maps[nx][ny] == "O":
+                            que.append((nx, ny, t+1))
+                            visited[nx][ny] = visited[x][y] + 1
+                        elif maps[nx][ny] == "E":
+                            return t+1
+                else:continue
     
-    def dfs(s_x, s_y):
-        x, y = s_x, s_y
-        
+    answer = bfs(s_x, s_y, 0)
 
-        
-        for k in range(4):
-            nx, ny = x + dx[k], y + dy[k]
-            if 0<=nx<r and 0<=ny<c:
-                if visited[nx][ny] == 0:
-                    if maps[nx][ny] == "L" or maps[nx][ny] == "O":
-                        visited[nx][ny] = visited[x][y] + 1
-                        dfs(nx, ny)
-                    elif maps[nx][ny] == "E":
-                        visited[nx][ny] = visited[x][y] + 1
-                        return 
-                        
-    dfs(s_x, s_y)
-    answer = visited[e_x][e_y] - 1
-
-        
-    return answer if answer > 0 else -1
+    return answer-1 if answer != None else -1
