a2ae78dc55df4def6d275fa62edcc6457e15dd09
이지영 <wldudwldhd@naver.com>
2023-06-07 23:28:34 +0900

    [PGS]정수삼각형/230607/DP/성공/다른풀이참고하기

--- jiyoung풀이/PGS문제/pgs_정수삼각형.py
+++ jiyoung풀이/PGS문제/pgs_정수삼각형.py
@@ -1,1 +1,28 @@
 # dp
+ # 내풀이
+def solution(triangle):
+    answer = 0
+    n = len(triangle)
+    for i in range(n):
+        for j in range(n):
+            triangle[i].append(0)
+
+
+    dp = [[0 for _ in range(n)] for _ in range(n)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, n):
+        for j in range(n):
+            if j==0:
+                dp[i][j] = dp[i-1][j]+triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+
+    answer = max(dp[-1][:])
+
+
+
+
+    return answer
