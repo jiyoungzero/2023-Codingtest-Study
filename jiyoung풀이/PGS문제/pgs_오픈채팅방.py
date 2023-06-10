161a03aea0fe6b9a3baa2c504a2e052a650a7247
이지영 <wldudwldhd@naver.com>
2023-06-11 02:01:37 +0900

    [PGS]오픈채팅방/230611/구현/성공

--- jiyoung풀이/PGS문제/pgs_오픈채팅방.py
+++ jiyoung풀이/PGS문제/pgs_오픈채팅방.py
@@ -1,2 +1,22 @@
 # 2019 카카오 기출
 
+def solution(record):
+    answer = []
+    nickname = {}
+
+    for r in record:
+        tmp = r.split()
+        if len(tmp) == 3:
+            cmd, id, name = tmp
+            nickname[id] = name
+
+    for r in record:
+        tmp = r.split()
+        if tmp[0] == "Enter":
+            name = nickname[tmp[1]]
+            answer.append(name+"님이 들어왔습니다.")
+        elif tmp[0] == "Leave":
+            name = nickname[tmp[1]]
+            answer.append(name+"님이 나갔습니다.")
+
+    return answer
