a1c3410c918b5b9ae1ffc2d4ef4604d6bc5a205d
이지영 <wldudwldhd@naver.com>
2023-06-14 23:00:19 +0900

    [PGS]주식가격/230614/스택,큐/성공

--- jiyoung풀이/PGS문제/pgs_주식가격.py
+++ jiyoung풀이/PGS문제/pgs_주식가격.py
@@ -1,14 +1,16 @@
+
 def solution(prices):
     answer = []
     n = len(prices)
-    
+
     for i in range(n):
         now = prices[i]
         tmp = 0
         for j in range(i, n):
-            if now <= prices[j]:
-                tmp+= 1
+            if now <= prices[j]:tmp += 1
+            else: 
+                tmp += 1
+                break
         answer.append(tmp-1)
-
 
     return answer
