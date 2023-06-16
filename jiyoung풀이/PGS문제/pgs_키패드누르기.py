fe7fdfbe025cdead7a0048ef300935b69fc99df3
이지영 <wldudwldhd@naver.com>
2023-06-16 23:50:34 +0900

    [PGS]키패드누르기/230616/구현/성공

--- jiyoung풀이/PGS문제/pgs_키패드누르기.py
+++ jiyoung풀이/PGS문제/pgs_키패드누르기.py
@@ -1,5 +1,35 @@
 def solution(numbers, hand):
     answer = ''
-    r_prev, r_now = 
-    l_prev, l_now = 
+    pad = [[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
+    l_pos, r_pos = [3,0],[3,2]
+
+    for i in range(len(numbers)):
+        if numbers[i] == 1 or numbers[i] == 4 or numbers[i] == 7:
+            answer += "L"
+            l_pos = [numbers[i]//3,0]
+        elif numbers[i] == 3 or numbers[i] == 6 or numbers[i] == 9:
+            answer += "R"
+            r_pos = [numbers[i]//3-1, 2]
+        else:
+            now_x, now_y = numbers[i]//3, 1
+            if numbers[i] == 0:
+                now_x, now_y = [3,1]
+            l_dist = abs(l_pos[0] - now_x) + abs(l_pos[1]-now_y)
+            r_dist = abs(r_pos[0] - now_x) + abs(r_pos[1]-now_y)
+            print(l_dist, r_dist)
+            if r_dist < l_dist:
+                answer += "R"
+                r_pos = [now_x, now_y]
+            elif r_dist > l_dist:
+                answer += "L"
+                l_pos = [now_x, now_y]
+            else:
+                if hand[0] == "r":
+                    answer += "R"
+                    r_pos = [now_x, now_y]
+                else:
+                    answer += "L"
+                    l_pos = [now_x, now_y]
+
+
     return answer
