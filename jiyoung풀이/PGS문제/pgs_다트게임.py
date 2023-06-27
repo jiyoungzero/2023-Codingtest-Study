379852419574d3319a751ff41d42171560f12b25
이지영 <wldudwldhd@naver.com>
2023-06-27 21:37:47 +0900

    [PGS]다트게임/230627/다른풀이참조/다시

--- jiyoung풀이/PGS문제/pgs_다트게임.py
+++ jiyoung풀이/PGS문제/pgs_다트게임.py
@@ -39,3 +39,34 @@ def solution(dartResult):
     print(result)
     return sum(result)
 # print(solution('1S*2T*3S'))
+
+
+
+# 다른 풀이 참조
+def solution(dartResult):
+    n = ''
+    score = []
+    for i in dartResult:
+        if i.isnumeric():
+            n += i
+        elif i == "S":
+            n = int(n)**1
+            score.append(n)
+            n = ''
+        elif i == "D":
+            n = int(n)**2
+            score.append(n)
+            n = ''
+        elif i == "T":
+            n = int(n)**3
+            score.append(n)
+            n = ''
+        elif i == "*":
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+        elif i == "#":
+            score[-1] = score[-1] * (-1)
+    return sum(score)
