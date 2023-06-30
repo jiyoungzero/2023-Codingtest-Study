8b5f53fb859cdb2b631d8cf98e5bccd2edf5d4a9
이지영 <wldudwldhd@naver.com>
2023-06-30 23:42:51 +0900

    [PGS]실패율/230630/정답/다시

--- jiyoung풀이/PGS문제/pgs_실패율.py
+++ jiyoung풀이/PGS문제/pgs_실패율.py
@@ -1,1 +1,23 @@
 # 2019 카카오 공채 문제
+from collections import Counter
+
+def solution(num, stage):
+    전체인원 = len(stage)
+    스테이지에_머물고_있는_사람 = Counter(stage)
+    
+    answer = []
+    
+    for i in range(1, num+1):
+        answer[i] = 0
+        
+    for i in answer:
+        if 스테이지에_머물고_있는_사람[i] != 0 and 전체인원 != 0:
+            answer[i] = 스테이지에_머물고_있는_사람[i]/전체인원
+        else:
+            answer[i] = 0
+        전체인원 -= 스테이지에_머물고_있는_사람[i]
+        
+    
+    return sorted(answer, key=lambda x:answer[x], reverse=True)
+
+# x:answer[x] :  value값 기준으로 정렬
