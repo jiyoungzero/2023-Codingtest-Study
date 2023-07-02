1562bed08be5bb37697fc29733c13bdb70c156c5
이지영 <wldudwldhd@naver.com>
2023-07-02 15:05:34 +0900

    [PGS]캐시/230702/부분점수/다시/구현

--- jiyoung풀이/PGS문제/pgs_캐시.py
+++ jiyoung풀이/PGS문제/pgs_캐시.py
@@ -1,1 +1,22 @@
 # 2018 카카오 블라인드
+
+# 75점 -> 왜??
+from collections import deque
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque()
+    if cacheSize == 0:
+        return 5*len(cities)
+    
+    for c in cities:
+        if c.lower() not in cache:
+            if len(cache) < cacheSize:
+                cache.append(c.lower())
+            else:
+                cache.popleft()
+                cache.append(c.lower())
+            answer += 5
+        else:
+            answer += 1
+
+    return answer
