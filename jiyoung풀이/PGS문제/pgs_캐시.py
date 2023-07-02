e972fdb462cdff8d7c722611b0e22de2c4d7811c
이지영 <wldudwldhd@naver.com>
2023-07-02 15:17:49 +0900

    [PGS]캐시/230702/정답

--- jiyoung풀이/PGS문제/pgs_캐시.py
+++ jiyoung풀이/PGS문제/pgs_캐시.py
@@ -1,13 +1,14 @@
 # 2018 카카오 블라인드
 
-# 75점 -> 왜??
+# 75점 -> 왜?? cacheHit 됏을때는 옛날의 버젼을 지우고 최신 버젼으로 업데이트 해주어야 LRU방식이 유지될 수 있다.!
+
 from collections import deque
 def solution(cacheSize, cities):
     answer = 0
     cache = deque()
     if cacheSize == 0:
         return 5*len(cities)
-    
+
     for c in cities:
         if c.lower() not in cache:
             if len(cache) < cacheSize:
@@ -17,6 +18,25 @@ def solution(cacheSize, cities):
                 cache.append(c.lower())
             answer += 5
         else:
+            cache.remove(c.lower())
             answer += 1
+            cache.append(c.lower())
 
     return answer
+
+
+# 다른 풀이 maxlen 중요 굿~
+def solution(cacheSize, cities):
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    time = 0
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
+        else:
+            cache.append(s)
+            time += 5
+    return time
