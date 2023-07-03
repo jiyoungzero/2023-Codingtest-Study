b46b9aa196aa0f4909a9fb3e6ac517f22d206545
이지영 <wldudwldhd@naver.com>
2023-07-03 22:42:46 +0900

    [PGS]프렌즈4블록/230703/다시

---
+++ jiyoung풀이/PGS문제/psg_프렌즈4블록.py
@@ -0,0 +1,15 @@
+# 2018 카카오 블라인드 
+
+import numpy as np
+
+def solution(m, n, board):
+    answer = 0
+    charac = ["R", "M"]
+    while True:
+        remove_lst = []
+        
+        for i in range(m-1):
+            for j in range(n-1):
+                for k in charac:
+    
+    return answer
