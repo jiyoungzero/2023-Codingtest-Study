2d707ac31c4759690adc24bd5ea66d9d5ba8cb43
이지영 <wldudwldhd@naver.com>
2023-07-22 01:01:35 +0900

    [이코테]개미전사/230721/재풀이

--- jiyoung풀이/이코테문제/book_개미전사2.py
+++ jiyoung풀이/이코테문제/book_개미전사2.py
@@ -3,3 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
+dp = [0]*(n+1)
+dp[0] = arr[0]
+dp[1] = max(arr[0], arr[1])
+
+# max(arr[i-2]+i, dp[i-1])
+for i in range(2, n):
+    dp[i] = max(dp[i-2]+arr[i], dp[i-1])
+    
+print(dp[n-1])
