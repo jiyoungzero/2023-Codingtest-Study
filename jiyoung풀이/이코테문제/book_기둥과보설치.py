a6906086dd30237d09fd06b69537bc132c77472e
이지영 <wldudwldhd@naver.com>
2023-08-20 23:54:12 +0900

    [이코테]기둥과보설치/230820/다시풀어보기

--- jiyoung풀이/이코테문제/book_기둥과보설치.py
+++ jiyoung풀이/이코테문제/book_기둥과보설치.py
@@ -1,6 +1,19 @@
 # 구현
 
 def possible(answer):
+    for x, y, stuff in answer:
+        # 기둥; 바닥 위에 있거나 보의 한쪽 끝 부분 위에 있거나, 또는 다른 기둥 위에 있어야 합니다.
+        if stuff  == 0:
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+                continue
+            else:
+                return False
+        # 보: 한쪽 끝 부분이 기둥 위에 있거나, 또는 양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 합니다.
+        elif stuff == 1:
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+                continue
+            else:
+                return False        
     return True
 
 def solution(n, build_frame):
