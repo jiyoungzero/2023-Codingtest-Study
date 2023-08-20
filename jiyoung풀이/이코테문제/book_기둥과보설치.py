7f03082d28cd87fa2bedc77281b81b2774e93448
이지영 <wldudwldhd@naver.com>
2023-08-20 23:36:44 +0900

    [이코테]기둥과보설치/230820/솔류션함수완성

--- jiyoung풀이/이코테문제/book_기둥과보설치.py
+++ jiyoung풀이/이코테문제/book_기둥과보설치.py
@@ -1,1 +1,19 @@
 # 구현
+
+def possible(answer):
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    
+    for frame in build_frame:
+        x, y, stuff,  operate = frame
+        if operate == 0: # 삭제
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+        if operate == 1 : # 설치
+            answer.append([x, y, stuff])
+            if not possible(answer):
+                answer.remove([x, y, stuff])
+    return sorted(answer)
