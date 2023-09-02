bf23ddee29593af560f35540487ebe5a17fa4158
이지영 <wldudwldhd@naver.com>
2023-09-03 00:41:18 +0900

    [ZB]보트/230902/풀이성공/다시

--- jiyoung풀이/ZB문제/보트.py
+++ jiyoung풀이/ZB문제/보트.py
@@ -8,3 +8,19 @@
 # 최대한 적게 보트를 사용하여 모든 사람을 구출하려고 할 때, 필요한 보트의 사용 횟수를 구하는 프로그램을 구현하세요.
 
 # 단, 무인도에 갇힌 사람은 1명 이상이며, 보트의 무게 제한보다 사람의 몸무게보다 큰 경우는 없습니다.
+def solution(A, limit): # 90 70 51 50 49 45
+    A.sort()
+    answer = 0
+    n = len(A)
+    visited = [False]*n
+
+    for i in range(n-1):
+        if visited[i] : continue
+        for j in range(n-1, i, -1):
+            if not visited[i] and not visited[j]: 
+                if A[i] + A[j] <= limit:
+                    print("ss", i, j,A[i] + A[j], limit)
+                    answer += 1
+                    visited[i], visited[j] = True, True
+
+    return (n - answer)
