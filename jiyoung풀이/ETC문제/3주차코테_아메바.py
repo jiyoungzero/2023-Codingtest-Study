366d5ddd573d48d0008f97d386cb5f2652b8804a
이지영 <wldudwldhd@naver.com>
2023-09-21 23:24:41 +0900

    [ZB]아메바/230921/다시

---
+++ jiyoung풀이/ETC문제/3주차코테_아메바.py
@@ -0,0 +1,18 @@
+def solution(delay, N):
+    answer = 1
+    que = []
+    que.append(0) # 시간 0
+
+    for _ in range(N):
+        temp = []
+        while que:
+            t = que.pop(0)
+            if t == 0:
+                temp.append(0)
+                temp.append(delay)
+                answer += 2
+            else:
+                temp.append(t-1)
+        que = temp #
+
+    return answer
