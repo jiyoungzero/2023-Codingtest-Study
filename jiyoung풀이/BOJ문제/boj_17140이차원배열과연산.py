6a314c578ccd271298ed164cdd590f85a1d6977f
이지영 <wldudwldhd@naver.com>
2023-09-27 22:39:00 +0900

    [BOJ]이차원배열과연산/230927/다시

--- jiyoung풀이/BOJ문제/boj_17140이차원배열과연산.py
+++ jiyoung풀이/BOJ문제/boj_17140이차원배열과연산.py
@@ -1,56 +1,60 @@
-import sys
-input = sys.stdin.readline
-from collections import Counter 
+r,c,k = map(int,input().split())
 
-r, c, k = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(3)]
-time = 0
-r, c = r-1, c-1
+A = [ list(map(int,input().split())) for _ in range(3) ]
 
-def R(a):
-    tmp = []
-    max_len = 0
-    for i in a:
-        row = []
-        target = (Counter(i))
-        target = list(target.items())
-        max_len = max(max_len, len(target)*2)
-        
-        target.sort(key=lambda x:(x[1], x[0]))
-        
-        for i in range(len(target)):
-            if i > 50 :break
-            else:
-                v, k = target[i]
-                if v == 0:break
-                row.append(v)
-                row.append(k)
-        tmp.append(row)
-    
-    # 나머지 부분은 0으로 채우기
-    for i in range(len(a)):
-        if len(tmp[i]) <= max_len:
-            for _ in range(max_len-len(tmp[i])):
-                tmp[i].append(0)
-    return tmp
-    
-    
-    
+def calR():
 
-def solution():
-    global arr, time
-    while True:
-        if arr[r][c] == k or time >= 100:
+    global A
+
+    new_graph = [] 
+
+    for i in A:
+
+        elem = set(i) # 한 행의 원소만 추출
+        temt = [] # (원소, 갯수) 를 담을 그래프
+        temt2 = [] # 새로운 행이 될 그래프
+        for j in elem:
+            if j==0:
+                continue
+            cnt = i.count(j)
+            temt.append((j,cnt))
+
+        temt.sort(key=lambda x:(x[1],x[0]))
+
+        for k in temt:
+            temt2.append(k[0])
+            temt2.append(k[1])
+
+        temt2 = temt2[:100] # 100개 제한
+
+        new_graph.append(temt2)
+
+
+    max_len = max(map(len,new_graph))
+
+	# 빈 칸은 0만큼 채워준다.
+    for i in range(len(new_graph)):
+        while len(new_graph[i]) !=max_len:
+            new_graph[i].append(0)
+
+    A = new_graph
+
+
+for i in range(101):
+
+    try:
+        if A[r-1][c-1]==k:
+            print(i)
             break
-        
-        if len(arr) >= len(arr[0]):
-            arr = R(arr)
-        else:
-            arr = list(map(list, zip(*arr)))
-            arr = R(arr)
-            arr = list(map(list, zip(*arr)))
-        time += 1
-    return time if time < 100 else -1
+    except:
+        pass
 
-print(solution())
-print(R(arr))
+    if len(A[0]) > len(A):
+        A = list(map(list,zip(*A)))
+        calR()
+        A = list(map(list,zip(*A)))
+    else:
+        calR()
+
+else:
+    print(-1)
