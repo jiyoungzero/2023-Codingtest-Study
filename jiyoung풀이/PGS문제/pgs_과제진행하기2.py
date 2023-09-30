7018094673290120df54f4c964454f2277a0d4d6
이지영 <wldudwldhd@naver.com>
2023-09-30 23:56:04 +0900

    [PGS]과제진행하기2/230930/다시

--- jiyoung풀이/PGS문제/pgs_과제진행하기2.py
+++ jiyoung풀이/PGS문제/pgs_과제진행하기2.py
@@ -1,1 +1,14 @@
 # 
+from collections import deque
+def solution(plans):
+    answer = []
+    que = deque()
+    jobs = []
+    
+    for p in plans: 
+        name, start, time = p[0], int(p[1][:2])*60+int(p[1][3:]), int(p[2])
+        jobs.append((name, start, time))
+        
+    print(jobs)
+    
+    return answer
