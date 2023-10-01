43bd35d24f798adc5e16cf88b8507fa3d75c4880
이지영 <wldudwldhd@naver.com>
2023-10-01 23:51:58 +0900

    [PGS]과제진행하기2/231001/다시

--- jiyoung풀이/PGS문제/pgs_과제진행하기2.py
+++ jiyoung풀이/PGS문제/pgs_과제진행하기2.py
@@ -9,6 +9,7 @@ def solution(plans):
         name, start, time = p[0], int(p[1][:2])*60+int(p[1][3:]), int(p[2])
         jobs.append((name, start, time))
         
+    jobs.sort(key = lambda x : x[1])
     print(jobs)
     
     return answer
