2b4e527899ef53f7313d62af0bb28a0a591661f5
이지영 <wldudwldhd@naver.com>
2023-10-03 01:16:30 +0900

    [PGS]과제진행하기2/231003/다시

--- jiyoung풀이/PGS문제/pgs_과제진행하기2.py
+++ jiyoung풀이/PGS문제/pgs_과제진행하기2.py
@@ -1,15 +1,38 @@
 # 
-from collections import deque
 def solution(plans):
     answer = []
-    que = deque()
-    jobs = []
-    
-    for p in plans: 
-        name, start, time = p[0], int(p[1][:2])*60+int(p[1][3:]), int(p[2])
-        jobs.append((name, start, time))
-        
-    jobs.sort(key = lambda x : x[1])
-    print(jobs)
-    
+
+    for i, v in enumerate(plans):
+        job, t1, t2 = v
+        hh, mm = t1.split(":")
+        plans[i][1], plans[i][2] = int(hh)*60+int(mm), int(t2)
+    plans.sort(key=lambda x:(x[1]))
+
+    stack = []
+    stack.append(plans[0])
+    now_time = plans[0][1] # 첫 작업 시간
+
+    for i in range(1, len(plans)):
+        next_time = plans[i][1] # 다음 작업 시작 시간
+
+        while len(stack):
+            job, time_start, time_spend = stack.pop()
+
+            if now_time < time_start: 
+                now_time = time_start
+
+            time_finish = now_time+time_spend
+
+            if next_time < time_finish:
+                stack.append([job, time_start, time_finish - next_time ])
+                now_time = next_time
+                break
+            else:
+                answer.append(job)
+                now_time += time_spend
+        stack.append(plans[i])
+
+    while len(stack):
+        answer.append(stack.pop()[0])
+
     return answer
