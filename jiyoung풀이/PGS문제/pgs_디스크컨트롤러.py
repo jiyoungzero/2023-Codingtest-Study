771994ec80a7bdac41beb0e060fbee9a6ee9bb92
이지영 <wldudwldhd@naver.com>
2023-10-06 23:26:38 +0900

    [PGS]디스크컨트롤러/231006/실패2

--- jiyoung풀이/PGS문제/pgs_디스크컨트롤러.py
+++ jiyoung풀이/PGS문제/pgs_디스크컨트롤러.py
@@ -1,14 +1,29 @@
 def solution(jobs):
     answer = 0
-    jobs.sort(key=lambda x:x[1])
-    
+    n = len(jobs)
+    jobs.sort(key=lambda x:x[0])
     now_time = jobs[0][0]+jobs[0][1]
     total = jobs[0][0]+jobs[0][1]
-    for i in range(1, len(jobs)):
-        if now_time > jobs[i][0]:
-            total += ((now_time-jobs[i][0])+jobs[i][1])
-        else:
-            total += t
-        now_time += jobs[i][1]
+    del jobs[0]
+    
+    while jobs:
+        disk = []
+        for i in range(len(jobs)):
+            if now_time >= jobs[i][0]:
+                disk.append(jobs[i])
+        if len(disk) == 0:
+            total += (jobs[0][0] + jobs[0][1])
+            now_time = jobs[0][0] + jobs[0][1]
+            del jobs[0]
             
-    return total // len(jobs)
+        else:
+            disk.sort(key=lambda x:x[1])
+            total += ((now_time-disk[0][0])+disk[0][1])
+            now_time += disk[0][1]
+            for i, (s, t) in enumerate(jobs):
+                if [s,t] == disk[0]:
+                    del jobs[i]
+
+    
+            
+    return total // n
