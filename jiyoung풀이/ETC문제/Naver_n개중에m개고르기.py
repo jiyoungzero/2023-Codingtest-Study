0cc089b4c1b1e7c15a287b08db5088cfae6156c6
이지영 <wldudwldhd@naver.com>
2023-11-06 22:37:11 +0900

    [ETC]n개중에m개고르기/231106/고르지않았을때기억하기/성공

--- jiyoung풀이/ETC문제/Naver_n개중에m개고르기.py
+++ jiyoung풀이/ETC문제/Naver_n개중에m개고르기.py
@@ -1,9 +1,6 @@
-# 백트래킹
-
 n, m = map(int, input().split())
 dots = [tuple(map(int, input().split())) for _ in range(n)]
 answer = int(1e9)
-dist = 0
 two_list = []
 
 
@@ -12,40 +9,31 @@ def get_distance(dot1, dot2):
     x2, y2 = dot2
     return (x1-x2)**2 + (y1-y2)**2
 
-# 고른 m개 중 2개의 조합 구하기
-def two_dots(sel, res, depth):
-    global two_list
-    if len(res) == 2:
-        two_list.append(res)
-        return 
-
-    if depth == len(sel):
-        return
-    for i in range(depth, len(sel)):
-        res.append(sel[i])
-        two_dots(sel, res, depth+1)
-        res.pop()
-    
 
 
 # 점 m개 고르기
 def backtracking(cnt, selected):
-    global answer, dots, result,dist
-    
+    global answer, dots, result
+
     if len(selected) == m:
-        two_dots(selected, [], 0)
-        for two in two_list:
-            d1, d2 = dots[0], dots[1]
-            dist = max(dist, get_distance(d1, d2))
+        dist = 0
+        for i in range(m-1):
+            for j in range(i+1, m):
+                dist = max(dist, get_distance(selected[i], selected[j]))
         answer = min(answer, dist)
         return 
 
 
-    if cnt == n:return 
+    if cnt >= n:return 
 
+    # cnt번째 선택 시
     selected.append(dots[cnt])
     backtracking(cnt+1, selected)
     selected.pop()
+
+    # cnt번째 선택 안할 시
+    backtracking(cnt+1, selected)
+    
 
 backtracking(0,[])
 print(answer)
