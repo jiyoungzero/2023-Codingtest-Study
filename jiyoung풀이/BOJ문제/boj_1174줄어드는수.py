f37ef014d75252be00e3964b72864dae61f13bc5
이지영 <wldudwldhd@naver.com>
2024-04-17 19:00:38 +0900

    [BOJ]줄어드는수/240417/성공

--- jiyoung풀이/BOJ문제/boj_1174줄어드는수.py
+++ jiyoung풀이/BOJ문제/boj_1174줄어드는수.py
@@ -1,33 +1,31 @@
 import sys 
 input = sys.stdin.readline 
-sys.setrecursionlimit(10**7)
 
 n = int(input())
-answer = -1
+result = set()
+num = []
 
-def check(num):
-    num_lst = [int(ele) for ele in str(num)]
-    l = len(num_lst)
-    for i in range(l):
-        for j in range(i+1, l):
-            if num_lst[i] <= num_lst[j]:
-                return False 
-    return True            
-        
+def backtracking():
+    global result, num
+    if len(num) > 0:
+        result.add(int("".join(map(str, num))))
 
-def backtracking(depth, cnt):
-    global answer
-    if cnt == n:
-        answer = depth-1
-        return 
-    if depth > 1000000:
-        answer = -1
-        return 
-    
-    if check(depth):
-        # print("check=true", depth)
-        backtracking(depth+1, cnt+1)
-    else:backtracking(depth+1, cnt)
-
-backtracking(0,0)
-print(answer)
+    for i in range(10):
+        if len(num) == 0:
+            num.append(i)
+            backtracking()
+            num.pop()
+                    else:
+            if num[-1] > i:
+                num.append(i)
+                backtracking()
+                num.pop()    
+                                
+try:
+    backtracking()
+    result = list(result)
+    result.sort()
+    # print(result)
+    print(result[n-1])
+except:
+    print(-1)
