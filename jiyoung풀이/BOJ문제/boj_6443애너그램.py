5b8cd425397cbc0c69862955cc548add120bea5c
이지영 <wldudwldhd@naver.com>
2024-04-17 19:57:46 +0900

    [BOJ]애너그램/240417/성공

--- jiyoung풀이/BOJ문제/boj_6443애너그램.py
+++ jiyoung풀이/BOJ문제/boj_6443애너그램.py
@@ -1,31 +1,30 @@
 import sys
+from collections import defaultdict
 input = sys.stdin.readline 
 
 n = int(input())
 
 for _ in range(n):
-    alphas = list(input().rstrip())
-    m = len(alphas)
-    result = set()
-    def backtracking(depth, sel):
-        if len(sel) == m:
-            tmp = ""
-            for j in sel:
-                tmp += alphas[j]
-            result.add(tmp)
-        if depth == len(alphas):
+    str_lst = list(input().rstrip())
+    alphas = defaultdict(int)
+    result = []
+    for st in str_lst:
+        alphas[st] += 1
+        
+    def dfs(cnt, cur_word):
+        global result
+        if cnt == len(str_lst):
+            result.append(cur_word)
             return 
-        
-        for i in range(m):
-            if i not in sel:
-                sel.append(i)
-                backtracking(depth+1, sel)
-                sel.pop()
-        
-        
-    backtracking(0, [])
-    result = list(result)
+            
+        for alp in alphas.keys():
+            if alphas[alp] > 0:
+                alphas[alp] -= 1
+                dfs(cnt+1, cur_word+[alp])
+                alphas[alp] += 1
+    dfs(0, [])
     result.sort()
     for r in result:
         print("".join(map(str, r)))
+            
         
