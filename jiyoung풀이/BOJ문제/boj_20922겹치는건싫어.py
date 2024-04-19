da28d4677ac67eb0b2b6839b050d5959ac037a0c
이지영 <wldudwldhd@naver.com>
2024-04-19 22:50:04 +0900

    [BOJ]겹치는건싫어/240419/성공

--- jiyoung풀이/BOJ문제/boj_20922겹치는건싫어.py
+++ jiyoung풀이/BOJ문제/boj_20922겹치는건싫어.py
@@ -7,24 +7,17 @@ arr = list(map(int ,input().split()))
 s, e = 0, 0
 answer = 0
 num_cnt = defaultdict(int)
-num_cnt[arr[s]] += 1
 
-def check():
-    for v in num_cnt.values():
-        if v > k:
-            return True
-    return False
-
-while e < n-1:
-    if not check():
-        e += 1
-        num_cnt[arr[e]] += 1
-    else:
-        print(s, e, answer)
-        answer = max((e-s), answer)        
-
+while e < n:
+    if num_cnt[arr[e]] >= k:        
         num_cnt[arr[s]] -= 1
         s += 1
+
+    else:
+        num_cnt[arr[e]] += 1  
+        e += 1
+        answer = max((e-s), answer)  
+
 print(answer)
 
         
