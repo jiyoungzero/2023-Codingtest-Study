84b6c65df1ab2918e833ebcea86b0ace58d984ef
이지영 <wldudwldhd@naver.com>
2024-04-21 12:03:56 +0900

    [BOJ]쉬운최단거리/240421/시간초과

--- jiyoung풀이/BOJ문제/boj_14940쉬운최단거리.py
+++ jiyoung풀이/BOJ문제/boj_14940쉬운최단거리.py
@@ -1,49 +1,53 @@
 import sys
 input = sys.stdin.readline 
+
 from collections import deque
 
-n, m = map(int ,input().split())
+n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-goal = (0,0)
-dxs,dys = [0,0,1,-1], [1,-1,0,0]
 result = [[-1]*m for _ in range(n)]
+# 0 : 갈수 없는 곳
+# 1: 갈 수 있는 곳
+# 2 : 목표지점
+
+# 원래 갈 수 있는데 못간 부분 : -1
+# 원래 갈 수 없는 곳 : 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 2:
-            goal = (i, j)
+        if arr[i][j] == 2 or arr[i][j] == 0:
             result[i][j] = 0
-        elif arr[i][j] == 0:
-            result[i][j] = 0
-        
-
+    
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
 
-def bfs(sx, sy):
-    global result
+def dfs(sx, sy):
+    dxs, dys = [0,0,1,-1], [1,-1,0,0]
     que = deque()
-    que.append((sx, sy))
-    visited = [[0]*m for _ in range(n)]
-    visited[sx][sy] = 0
-
+    visited = [[False]*m for _ in range(n)]
+    que.append((sx, sy, 0))
+    visited[sx][sy] = True
+    
     while que:
-        x, y = que.popleft()
-        if goal == (x, y):
-            result[sx][sy] = visited[x][y]
+        x, y, dist = que.popleft()
+        if arr[x][y] == 2:
+            result[sx][sy] = dist
             return 
-            
         for dir in range(4):
             nx, ny = x + dxs[dir], y + dys[dir]
-            if in_range(nx, ny):
-                if arr[nx][ny] == 1 and visited[nx][ny] == -1:
-                    visited[nx][ny] = visited[x][y] + 1
-                    que.append((nx, ny))
-    
+            if not in_range(nx, ny):continue
+            if not visited[nx][ny] and arr[nx][ny] != 0:
+                visited[nx][ny] = True
+                que.append((nx, ny, dist+1))
+
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 1:
-            bfs(i, j)
+            dfs(i,j)
+
 for row in result:
     print(*row)
+            
         
+    
+
