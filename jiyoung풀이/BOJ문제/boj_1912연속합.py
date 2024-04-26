9d88c5d1dcf24607d3cf3e346b3161b4ec908ded
이지영 <wldudwldhd@naver.com>
2024-04-26 22:11:58 +0900

    [BOJ]연속합/240426/DP/다시

---
+++ jiyoung풀이/BOJ문제/boj_1912연속합.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+arr = list(map(int ,input().split()))
+
+for i in range(1, n):
+    arr[i] = max(arr[i], arr[i]+arr[i-1])
+print(max(arr))
