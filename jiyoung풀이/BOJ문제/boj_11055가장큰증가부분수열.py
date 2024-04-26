fbc2fa7281b786999f386df5071cc89a9907b2e3
이지영 <wldudwldhd@naver.com>
2024-04-26 22:56:32 +0900

    [BOJ]가장큰증가부분수열/240426/성공

---
+++ jiyoung풀이/BOJ문제/boj_11055가장큰증가부분수열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [0]*n
+dp[0] = arr[0]
+
+for i in range(1, n):
+      for j in range(i):
+            if arr[i] > arr[j]:
+                  dp[i] = max(dp[i], arr[i]+dp[j])
+            else:
+                  dp[i] = max(dp[i], arr[i])
+print(max(dp))
