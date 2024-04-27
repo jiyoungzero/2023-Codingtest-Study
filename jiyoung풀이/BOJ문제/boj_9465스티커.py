f3929618377da00fbf782a0b0c1280fdfdf4fbae
이지영 <wldudwldhd@naver.com>
2024-04-28 00:25:55 +0900

    [BOJ]스티커/240427/DP/성공

--- jiyoung풀이/BOJ문제/boj_9465스티커.py
+++ jiyoung풀이/BOJ문제/boj_9465스티커.py
@@ -5,17 +5,18 @@ T = int(input())
 for _ in range(T):
     f_flag, s_flag = False, False
     n = int(input())
-    first = list(map(int, input().split()))
-    second = list(map(int, input().split()))
+    lst = [list(map(int, input().split())) for _ in range(2)]
     
-    dp = [0]*n
-    dp[0] = max(first[0], second[0])
-    selected = [[False]*2 for _ in range(n)]
     if n > 1:
-        dp[1] = max(first[0]+second[1], second[0]+first[1])
-        if first[0] + second[1] > second[0] + first[1]:
-            selected[1][1] = True
-        for i in range(2, n):
-            dp[i] = max(max(first[i], second[i])+dp[i-2], dp[i-2]+first[i] if selected[i-1][0] else second[i])
-    print(dp[-1])
+        lst[0][1] += lst[1][0]
+        lst[1][1] += lst[0][0]
+
+    for i in range(2, n):
+        lst[0][i] += max(lst[1][i-1], lst[1][i-2])
+        lst[1][i] += max(lst[0][i-1], lst[0][i-2])
+
+    print(max(lst[0][-1], lst[1][-1]))    
+        
+
+    
             
