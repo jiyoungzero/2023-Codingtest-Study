909eaa20232fd72f70c2f1a02a0877fa1c52ab7c
이지영 <wldudwldhd@naver.com>
2024-04-29 22:52:41 +0900

    [BOJ]스티커/240429/리팩토링

--- jiyoung풀이/BOJ문제/boj_9465스티커.py
+++ jiyoung풀이/BOJ문제/boj_9465스티커.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
-    f_flag, s_flag = False, False
     n = int(input())
     lst = [list(map(int, input().split())) for _ in range(2)]
     
@@ -15,8 +14,7 @@ for _ in range(T):
         lst[0][i] += max(lst[1][i-1], lst[1][i-2])
         lst[1][i] += max(lst[0][i-1], lst[0][i-2])
 
-    print(max(lst[0][-1], lst[1][-1]))    
-        
+    print(max(lst[0][-1], lst[1][-1]))
 
     
             
