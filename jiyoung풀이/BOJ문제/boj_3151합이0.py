fb791515e9db4e1ba604cc80635c762c705bcf4d
이지영 <wldudwldhd@naver.com>
2024-05-02 00:16:46 +0900

    [BOJ]합이0/240501/이분탐색,투포인터/성공_다시/테스트케이스

--- jiyoung풀이/BOJ문제/boj_3151합이0.py
+++ jiyoung풀이/BOJ문제/boj_3151합이0.py
@@ -1,7 +1,26 @@
 import sys
 input = sys.stdin.readline 
+from bisect import bisect_left
 
 N = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
-print(arr)
+answer = 0
+
+for i in range(N-2):
+    left, right = i+1, N-1
+    while left < right:
+        tmp = arr[i] + arr[left] + arr[right]
+        if tmp > 0:
+            right -= 1
+        else:
+            if tmp == 0:
+                # print(i, left, right)
+                if arr[left] == arr[right]:
+                    answer += (right - left)
+                else:
+                    idx = bisect_left(arr, arr[right])
+                    answer += (right-idx +1)
+            left += 1
+print(answer)
+            
