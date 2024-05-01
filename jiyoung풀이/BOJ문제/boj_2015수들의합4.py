e1424aaa4ea9f1b8000ebd5adb7f4b56987596d1
이지영 <wldudwldhd@naver.com>
2024-05-02 01:06:25 +0900

    [BOJ]수들의합4/240502/성공/다시

--- jiyoung풀이/BOJ문제/boj_2015수들의합4.py
+++ jiyoung풀이/BOJ문제/boj_2015수들의합4.py
@@ -1,21 +1,44 @@
 import sys
 input = sys.stdin.readline 
+from collections import defaultdict
 
+
+# 누적합 : 배열이 아닌, 딕셔너리로 풀이
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-prefix = [0]*n
-prefix[0] =arr[0]
 answer = 0
+prefix = [0]
+for i in range(n):
+    prefix.append(prefix[-1] + arr[i])
 
-for i in range(1, n):
-    prefix[i] = prefix[i-1] + arr[i]
-prefix = [0] + prefix
+dict = defaultdict(int)
+for i in range(n+1):
+    answer += dict.get(prefix[i]-k, 0)
+    dict[prefix[i]] += 1
 
-print(prefix)
-for i in range(1, n+1):
-    for j in range(i, n+1):
-        if prefix[j] - prefix[i-1] == k:
-            # print(i, j)
-            answer += 1
 print(answer)
+    
+
+
+
+
+
+# @@@ 시간초과 
+# prefix = [0]*n
+# prefix[0] =arr[0]
+# answer = 0
+
+# for i in range(1, n):
+#     prefix[i] = prefix[i-1] + arr[i]
+# prefix = [0] + prefix
+
+# # print(prefix)
+# for i in range(1, n+1):
+#     for j in range(i, n+1):
+#         if prefix[j] - prefix[i-1] == k:
+#             # print(i, j)
+#             answer += 1
+# print(answer)
+
+
     
