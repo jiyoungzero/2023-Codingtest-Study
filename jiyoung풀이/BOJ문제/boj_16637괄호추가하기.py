aa84befa448c108e833bdaa6b90e2ffe765a4f5e
이지영 <wldudwldhd@naver.com>
2024-05-05 14:21:13 +0900

    [BOJ]괄호추가하기/240505/성공/다시

--- jiyoung풀이/BOJ문제/boj_16637괄호추가하기.py
+++ jiyoung풀이/BOJ문제/boj_16637괄호추가하기.py
@@ -5,47 +5,32 @@ n = int(input())
 lst = input().rstrip()
 answer = -int(1e9)
 
-cmd_len = n//2
-
-# 0 -> 0, 2 -> 1
-# 1 -> 2, 4 -> 3
-# 2 -> 4, 6 -> 5
-def calculate(cmd_f):
-    result = 0
-    for i, f in enumerate(cmd_f):
-        if f:
-            l, r = i*2, l+2
-            # 실제 
-            idx = i*2 + 1
-            if lst[idx] == '+':
-                result += (lst[l]+lst[r])
-            elif lst[idx] == '*':
-                result += (lst[l]*lst[r])
-            else:
-                result += (lst[l]-lst[r])
-    
-    for
-                
-            
-            
-            
-    pass
-
-def backtracking(depth, cmd_flag):
-    global answer
-    if depth == cmd_len:
-        answer = max(answer, calculate(cmd_flag))
-        return 
-    if depth > 0 and cmd_flag[depth-1]:
-        cmd_flag[depth] = True
-        backtracking(depth+1,cmd_flag)
-        cmd_flag[depth] = False
+def calculate(a, op, b):
+    if op == '*':
+        return a * b
+    elif op == '+':
+        return a + b
     else:
-        cmd_flag[depth] = True
-        backtracking(depth+1, cmd_flag)
-        cmd_flag[depth] = False
-        
-        backtracking(depth+1, cmd_flag)
-
-backtracking(0, [False]*cmd_len)
+        return a - b
     
+    
+def backtracking(idx, value): # 현재 인덱스와 남은 계산식
+    global answer
+    if idx == n-1:
+        answer = max(answer, value)
+        return
+    
+    # 현재 괄호를 쓴다면
+    if idx+4 < n:
+        nxt_nxt_value = calculate(int(lst[idx+2]), lst[idx+3], int(lst[idx+4]))
+        nxt_value = calculate(value, lst[idx+1], nxt_nxt_value)
+        backtracking(idx+4, nxt_value)
+    
+    # 현재 괄호를 안쓴다면
+    if idx+2 < n:
+        nxt_value = calculate(value, lst[idx+1], int(lst[idx+2]))
+        backtracking(idx+2, nxt_value)
+    
+backtracking(0, int(lst[0]))
+print(answer)
+
