283c751298f30d973ea6482125ffd5b6e9580d7a
이지영 <wldudwldhd@naver.com>
2024-05-08 00:13:57 +0900

    [BOJ]카드섞기/240507/실패

--- jiyoung풀이/BOJ문제/boj_21315카드섞기.py
+++ jiyoung풀이/BOJ문제/boj_21315카드섞기.py
@@ -4,17 +4,27 @@ input = sys.stdin.readline
 n = int(input())
 result = map(int, input().split())
 answer = []
-# 초기상태
-cards = [i for i in range(1, n+1)]
+
 
 def simulate(sel):
-    pass
+    # 초기상태
+    cards = [i for i in range(1, n+1)]
+    for num in sel:
+        before= cards[1:]
+        cards = cards[1:] + cards[0]
+        for i in range(2, num):
+            ups = before[-2**(num-i+1):]
+            cards = ups + ups[:2**(num-i+1)] + cards[0]
+            before = ups
+    for a, b in zip(result, cards):
+        if a != b:
+            return False
+    return True
 
 
 def select_2num(sel, idx):
     global answer
     if len(sel) == 2:
-        print(sel)
         if simulate(sel):
             answer = sel
             return 
@@ -22,12 +32,13 @@ def select_2num(sel, idx):
     if idx == n+1:
         return 
     
+    for i in range(1, n+1):
+        if i not in sel:
+            select_2num(sel+[i], idx+1)
 
-    select_2num(sel+[idx], idx+1)
-    
-    select_2num(sel, idx+1)
 
 select_2num([], 1)
+print(*answer)
 
             
         
