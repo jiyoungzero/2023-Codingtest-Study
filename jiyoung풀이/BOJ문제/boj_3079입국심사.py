6570a1c1bbb4b24d1342d5971834c6a69f3c13af
이지영 <wldudwldhd@naver.com>
2024-05-09 16:37:53 +0900

    [BOJ]입국심사/240509/이분탐색/오답

---
+++ jiyoung풀이/BOJ문제/boj_3079입국심사.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int ,input().split())
+tk = [int(input()) for _ in range(n)]
+answer = int(1e9)
+
+# 인원이 10**9 -> 그리디, 완전탐색(내가 생각한 거)는 시간초과
+# 이분탐색으로 가자 -> 완탐을 거의 대체할 수 있다. 
+l, r = min(tk), max(tk)*m
+
+while l <= r:
+    mid = (l+r)//2
+    total = 0
+    for t in tk:
+        total += (mid//t)
+    
+    if total >= m:
+        answer = min(answer, mid)
+        r = mid - 1
+    else:
+        l = mid +1 
+        
+print(answer)
+        
+            
+    
