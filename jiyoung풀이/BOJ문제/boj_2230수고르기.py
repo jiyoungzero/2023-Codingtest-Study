bb713f4bace2fe16661568003403ed2087dac485
이지영 <wldudwldhd@naver.com>
2024-05-09 23:20:30 +0900

    [BOJ]수고르기/240509/리팩토링

--- jiyoung풀이/BOJ문제/boj_2230수고르기.py
+++ jiyoung풀이/BOJ문제/boj_2230수고르기.py
@@ -7,13 +7,12 @@ answer = int(1e9)*2+1
 arr.sort()
 
 a, b = 0, 1
-while a < b and b < n:
+while a <= b and b < n:
     sub = abs(arr[a] - arr[b])
     if sub >= m :
         answer = min(answer, sub)
         a += 1
-        if a == b:
-            b += 1
+
     else:
         b += 1
 print(answer)
