eb77c83b323c3a4459efd7ccf1413bc84e303250
이지영 <wldudwldhd@naver.com>
2024-05-10 23:47:54 +0900

    [BOJ]N과M12/240510/시간초과

--- jiyoung풀이/BOJ문제/boj_15666N과M12.py
+++ jiyoung풀이/BOJ문제/boj_15666N과M12.py
@@ -4,25 +4,25 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 answer = []
+arr.sort() # 1 7 9 9
 
 # 오름차순, 같은 수 가능, m개 고르기
 def backtracking(sel, depth):
     global answer
     if len(sel) == m:
-        print(sel)
-        return 
+        if sel not in answer:
+            answer.append(sel)
+        return
     
     if depth == n:
         return 
     
-    if len(sel) == 0:
-        sel.append(arr[depth])
-        backtracking(sel, depth+1)
-        sel.pop()
-    else:
-        if sel[-1] <= arr[depth]:
-            backtracking(sel+[arr[depth]], depth+1)
-        
+    for i in range(n):
+        if len(sel) == 0 or sel[-1] <= arr[i]:
+            backtracking(sel+[arr[i]], depth+1)
         backtracking(sel, depth+1)
 backtracking([], 0)
+answer.sort()
+for ele in answer:
+    print(*ele)
 
