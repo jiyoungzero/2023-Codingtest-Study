f50a7d5a4c08abb13e169765a55b88cfe273b370
이지영 <wldudwldhd@naver.com>
2024-05-11 00:02:58 +0900

    [BOJ]N과M12/240510/성공

--- jiyoung풀이/BOJ문제/boj_15666N과M12.py
+++ jiyoung풀이/BOJ문제/boj_15666N과M12.py
@@ -2,27 +2,42 @@ import sys
 input = sys.stdin.readline 
 
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-answer = []
-arr.sort() # 1 7 9 9
+arr = sorted(set(list(map(int, input().split()))))
 
-# 오름차순, 같은 수 가능, m개 고르기
-def backtracking(sel, depth):
-    global answer
-    if len(sel) == m:
-        if sel not in answer:
-            answer.append(sel)
-        return
-    
-    if depth == n:
+N = len(arr)
+answer = []
+
+def backtracking(depth, idx):
+    if depth == m:
+        print(' '.join(map(str, answer)))
         return 
     
-    for i in range(n):
-        if len(sel) == 0 or sel[-1] <= arr[i]:
-            backtracking(sel+[arr[i]], depth+1)
-        backtracking(sel, depth+1)
-backtracking([], 0)
-answer.sort()
-for ele in answer:
-    print(*ele)
+    for i in range(idx, len(arr)):
+        answer.append(arr[i])
+        backtracking(depth+1, i)
+        answer.pop()
+        
+        # backtracking(depth+1, i)
+
+backtracking(0,0)
+
+# 오름차순, 같은 수 가능, m개 고르기
+# def backtracking(sel, depth):
+#     global answer
+#     if len(sel) == m:
+#         if sel not in answer:
+#             answer.append(sel)
+#         return
+    
+#     if depth == n:
+#         return 
+    
+#     for i in range(n):
+#         if len(sel) == 0 or sel[-1] <= arr[i]:
+#             backtracking(sel+[arr[i]], depth+1)
+#         backtracking(sel, depth+1)
+# backtracking([], 0)
+# answer.sort()
+# for ele in answer:
+#     print(*ele)
 
