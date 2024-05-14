bd734900da4340445479f7d3524dcc72791121d2
이지영 <wldudwldhd@naver.com>
2024-05-15 00:21:11 +0900

    [BOJ]피아노체조/240515/성공

--- jiyoung풀이/BOJ문제/boj_21318피아노체조.py
+++ jiyoung풀이/BOJ문제/boj_21318피아노체조.py
@@ -5,5 +5,16 @@ n = int(input())
 arr = list(map(int, input().split()))
 q = int(input())
 
+prefix = [0]*n
+for i in range(1, n):
+    if arr[i] < arr[i-1]:
+        prefix[i] = prefix[i-1] + 1
+    else:
+        prefix[i] = prefix[i-1]
+
 for _ in range(q):
     a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    print(prefix[b]-prefix[a])
+    
