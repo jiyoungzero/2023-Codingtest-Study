4bf6450b2c6d00653fcbceae3858d4b5ddbf32fd
이지영 <wldudwldhd@naver.com>
2024-05-16 23:47:56 +0900

    B[BOJ]일루미네이션/240516/실패

--- jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
+++ jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
@@ -1,3 +1,38 @@
 import sys
-input = sys.stdin.readline 
+input = sys.stdin.readline
+from collections import deque 
 
+w, h = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(h)]
+answer = 0
+dxs, dys = [-1, 0, 1, 0, -1, -1], [-1, -1, 0, 1, 1, 0 ]
+visited = [[False]*w for _ in range(h)]
+
+def bfs(x, y):
+    global answer
+    que = deque()
+    visited[x][y] = True
+    que.append((x, y))
+    
+    while que:
+        x, y = que.popleft()
+        for k in range(6):
+            nx, ny = x + dxs[k], y + dys[k]
+            if nx < 0 or ny < 0 or h <= nx or w <= ny: 
+                answer += 1
+                continue
+            
+            if arr[nx][ny] == 0:
+                answer += 1
+                continue
+
+            if arr[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                que.append((nx, ny))
+
+for i in range(h):
+    for j in range(w):
+        if arr[i][j] == 1 and not visited[i][j]:
+            bfs(i, j)
+            
+print(answer)
