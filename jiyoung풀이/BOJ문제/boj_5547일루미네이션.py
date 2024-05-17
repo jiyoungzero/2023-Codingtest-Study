dcaf88a208c8cd97f03f9cae3677df8f481e9bf2
이지영 <wldudwldhd@naver.com>
2024-05-17 23:21:03 +0900

    [BOJ]일루미네이션/240517/실패/규칙찾기

--- jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
+++ jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
@@ -2,6 +2,11 @@ import sys
 input = sys.stdin.readline
 from collections import deque 
 
+# 전체 좌표 평면의 외곽에 1만큼의 여백을 추가하고 x,y 좌표가 0부터 시작한다고 판단
+# y가 홀수 일 때, 인접한 좌표는 상하좌우와 함께 우상단,우하단을 포함
+# y가 짝수 일 때, 인접한 좌표는 상하좌우와 함께 좌상단, 좌하단을 포함
+# 건물이 없는 좌표를 BFS 탐색하면서 건물과 만나는 지점을 카운트
+
 w, h = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(h)]
 answer = 0
@@ -20,13 +25,12 @@ def bfs(x, y):
             nx, ny = x + dxs[k], y + dys[k]
             if nx < 0 or ny < 0 or h <= nx or w <= ny: 
                 answer += 1
-                continue
             
-            if arr[nx][ny] == 0:
+            elif arr[nx][ny] == 0:
                 answer += 1
-                continue
 
-            if arr[nx][ny] == 1 and not visited[nx][ny]:
+
+            elif arr[nx][ny] == 1 and not visited[nx][ny]:
                 visited[nx][ny] = True
                 que.append((nx, ny))
 
