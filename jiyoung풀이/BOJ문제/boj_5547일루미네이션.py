a15da6751ba087ff9a434a05e38d9b0421c8a6b9
이지영 <wldudwldhd@naver.com>
2024-05-21 21:21:10 +0900

    [BOJ]일루미네이션/230521/실패

--- jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
+++ jiyoung풀이/BOJ문제/boj_5547일루미네이션.py
@@ -10,5 +10,34 @@ from collections import deque
 w, h = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(h)]
 answer = 0
-dxs = [-1, 0, 1, 0, -1, -1]
+dxs = [[-1, 0, 1, 0, -1, -1], [0,1,1,1,0,-1]] # 홀수, 짝수
 dys = [-1, -1, 0, 1, 1, 0 ]
+visited = [[False for _ in range(w+2)] for _ in range(h+2)]
+
+
+# 회색 = 건물 = 1
+# 빈공간 = 0
+def bfs(sy, sx): 
+    global answer
+    
+    que = deque()
+    que.append((sy,sx))
+    visited[sy][sx] = True
+    while que:
+        y, x = que.popleft()
+
+        for k in range(6):
+            nx, ny = x + dxs[y%2][k], y + dys[k]
+            if 0 <= ny < (h+2) and 0 <= nx < (w+2):
+                if arr[ny][nx] == 1:
+                    answer += 1
+                else:
+                    if not visited[ny][nx]:
+                        visited[ny][nx] = True
+                        que.append((ny, nx))
+                    
+
+    
+    
+bfs(0,0)
+print(answer)
