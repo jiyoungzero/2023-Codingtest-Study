0cf5bbe767b20c558f7fc82d294de5b7a109e207
이지영 <wldudwldhd@naver.com>
2024-05-25 23:35:16 +0900

    [BOJ]N과M9/240525/보완

--- jiyoung풀이/BOJ문제/boj_15663N과M9.py
+++ jiyoung풀이/BOJ문제/boj_15663N과M9.py
@@ -3,23 +3,45 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
+lst.sort()
 
-answer = set()
-used = [False]*n
-def backtracking(sel):
-    global answer
-    if len(sel) == m :
-        answer.add(tuple(sel))
-        return 
+# 내 풀이
+# answer = set()
+# used = [False]*n
+# def backtracking(sel):
+#     global answer
+#     if len(sel) == m :
+#         answer.add(tuple(sel))
+#         return 
         
     
+#     for i in range(n):
+#         if used[i]:continue
+#         used[i] = True
+#         backtracking(sel + [lst[i]])
+#         used[i] = False
+
+
+# backtracking([])
+# for ele in sorted(list(answer)):
+#     print(' '.join(map(str, ele)))
+sel = []
+used = [False]*n # [1, 7, 9, 9]
+def backtracking():
+    if len(sel) == m:
+        print(*sel)
+        return 
+    
+    overlap = 0
     for i in range(n):
-        if used[i]:continue
-        used[i] = True
-        backtracking(sel + [lst[i]])
-        used[i] = False
-
-
-backtracking([])
-for ele in sorted(list(answer)):
-    print(' '.join(map(str, ele)))
+        if used[i]: continue
+        if overlap != lst[i]:
+            used[i] = True
+            sel.append(lst[i])
+            overlap = lst[i]
+            print(sel, overlap)
+            backtracking()
+            
+            sel.pop()
+            used[i] = False
+backtracking()
