0dd077b81055e1f8f746feb03c5194e3ba9be2f3
이지영 <wldudwldhd@naver.com>
2024-05-27 23:59:23 +0900

    [BOJ]귀여운라이언/240527/다시

--- jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
+++ jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
@@ -8,15 +8,23 @@ answer = int(1e9)
 
 # 1이 k개 이상 포함된, 연속된 가장 작은 길이
 s, e = 0, 0
-cnt = 1 if arr[s] == 1 else 0
-while e < n:
-    e += 1
+cnt = 0
+while e < n:    
+    if arr[e] == 1:
+        cnt += 1
+        
     if cnt >= k:
         if answer > e-s+1:
             answer = e-s+1
-            s += 1
-    
-    if arr[e] == 1:
-        cnt += 1
+            print(s, e)
+            cnt = 0
+            
+            while s != e:
+                s += 1
+                if arr[s] == 1:
+                    cnt += 1
+                    
+    e += 1
+    print(s, e)
     
 print(answer if answer < int(1e9) else -1)
