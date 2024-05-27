42496576884f03ca28b131ace879c0dc8dbf12c7
이지영 <wldudwldhd@naver.com>
2024-05-27 23:46:53 +0900

    [BOJ]귀여운라이언/240527/실패

--- jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
+++ jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
@@ -2,3 +2,21 @@ import sys
 input = sys.stdin.readline 
 
 
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+answer = int(1e9)
+
+# 1이 k개 이상 포함된, 연속된 가장 작은 길이
+s, e = 0, 0
+cnt = 1 if arr[s] == 1 else 0
+while e < n:
+    e += 1
+    if cnt >= k:
+        if answer > e-s+1:
+            answer = e-s+1
+            s += 1
+    
+    if arr[e] == 1:
+        cnt += 1
+    
+print(answer if answer < int(1e9) else -1)
