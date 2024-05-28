2b9638f7e706cabe2e341b441d3f3a5ba665c2a1
이지영 <wldudwldhd@naver.com>
2024-05-28 22:29:12 +0900

    [BOJ]귀여운라이언/240528/실패

--- jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
+++ jiyoung풀이/BOJ문제/boj_15565귀여운라이언.py
@@ -8,24 +8,19 @@ answer = int(1e9)
 
 # 1이 k개 이상 포함된, 연속된 가장 작은 길이
 s, e = 0, 0
-cnt = 0
-while e < n:    
-    if arr[e] == 1:
-        cnt += 1
-        
-    if cnt >= k:
-        if answer > e-s+1:
-            answer = e-s+1
-            # print(s, e)
-            s += 1
-            cnt = 0
-            
-            while arr[s] != 1:
-                s += 1
-            e = s
-            cnt = 1
+cnt = 1 if arr[s] == 1 else 0
+# 왼쪽 포인터, 오른쪽 포인터 둘 중 하나가 끝에 도달할 때까지 반복한다.
+while e < n:
+    # 라이언 인형의 개수가 부족하다면, 오른쪽 포인터를 옮겨 본다.
+    if cnt < k:
+        e += 1
+        if e < n and arr[e] == 1:
+            cnt += 1
+    # 라이언 인형의 개수가 충분하다면, 왼쪽 포인터를 옮겨 본다.
+    else:
+        answer = min(answer, (e-s+1))
+        s += 1
+        if arr[s-1] == 1:
+            cnt -= 1
 
-                    
-    e += 1
-    
-print(answer if answer < int(1e9) else -1)
+print(answer)
