fbb23c148afcd0c587e8b22ef7c0958b9e1691bd
이지영 <wldudwldhd@naver.com>
2024-05-30 15:21:44 +0900

    [BOJ]별찍기10/240530/다시

--- jiyoung풀이/BOJ문제/boj_2447별찍기10.py
+++ jiyoung풀이/BOJ문제/boj_2447별찍기10.py
@@ -1,2 +1,28 @@
 import sys
 input = sys.stdin.readline 
+
+n = int(input())
+
+# 분할정복 + 재귀 
+def recur(n):
+    if n == 1:
+        return "*"
+    
+    stars = recur(n//3)
+    answer = []
+    
+    for s in stars:
+        answer.append(s*3)
+    
+    for s in stars:
+        answer.append(s + " "*(n//3) + s)
+    
+    for s in stars:
+        answer.append(s*3)
+    
+    return answer
+
+answer = recur(n)
+for ele in answer:
+    print(ele)
+    
