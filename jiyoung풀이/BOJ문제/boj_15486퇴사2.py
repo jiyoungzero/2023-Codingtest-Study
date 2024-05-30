29440b1dba1c6667aa9905ea4695af51010809ed
이지영 <wldudwldhd@naver.com>
2024-05-30 13:19:23 +0900

    [BOJ]퇴사2/240530/성공

--- jiyoung풀이/BOJ문제/boj_15486퇴사2.py
+++ jiyoung풀이/BOJ문제/boj_15486퇴사2.py
@@ -6,3 +6,18 @@ arr = []
 for _ in range(n):
     t, p = map(int, input().split())
     arr.append((t, p))
+arr.insert(0, (0,0))
+# print(arr)
+    
+# 탑 다운 방식 활용
+dp = [0]*(n+1) # 초기값 
+dp[n] = 0 if arr[n][0] > 1 else arr[n][1]
+for i in range(n-1, 0, -1):
+    if i+arr[i][0] >= n+2:
+        dp[i] = dp[i+1]
+    elif i+arr[i][0] == n+1:
+        dp[i] = max(arr[i][1], dp[i+1])
+    else:
+        dp[i] = max(dp[i+arr[i][0]]+arr[i][1], dp[i+1])
+print(dp)
+print(dp[1])
