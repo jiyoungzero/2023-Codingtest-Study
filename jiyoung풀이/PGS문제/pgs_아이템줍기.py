6bc257ffd7b63d2206513ab029081690f7daa143
이지영 <wldudwldhd@naver.com>
2024-06-06 15:40:08 +0900

    [PGS]아이템줍기/240606/성공

--- jiyoung풀이/PGS문제/pgs_아이템줍기.py
+++ jiyoung풀이/PGS문제/pgs_아이템줍기.py
@@ -1,16 +1,40 @@
+from collections import deque 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    arr = [[-1]*51 for _ in range(51)]
+    arr = [[-1]*102 for _ in range(102)]
+    characterX, characterY, itemX, itemY = characterX*2, characterY*2, itemX*2, itemY*2
+    
+    # 빈칸 -1, 둘레 1, 사각형 안쪽 0
     for rec in rectangle:
-        sc, sr, ec, er = rec
-        for i in range(sr, er+1):
-            for j in range(sc, ec+1):
-                if arr[i][j] == 0:continue
-                if i == sr or i == er or j == sc or j == ec:
-                    arr[i][j] = 1
-                else:
+        sy, sx, ey, ex = rec
+        sy, sx, ey, ex = sy*2, sx*2, ey*2, ex*2
+        for i in range(sx, ex+1):
+            for j in range(sy, ey+1):
+                # 이미 내부라고 되어있다면 continue
+                if arr[i][j] == 0: continue 
+                elif sx < i < ex and sy < j < ey:
                     arr[i][j] = 0
-    for row in arr:
-        print(*row[:11])
-                    
+                else:
+                    arr[i][j] = 1
+    
+    dxs, dys = [0,0,1,-1],[1,-1,0,0]
+    visited = [[1]*102 for _ in range(102)]
+    
+    que = deque()
+    que.append((characterY, characterX))
+    
+    while que:
+        x, y = que.popleft()
+        if (x, y) == (itemY, itemX):
+            
+            answer = visited[x][y] // 2
+            break
+        for k in range(4):
+            nx, ny = x + dxs[k], y + dys[k]
+            if nx < 0 or ny < 0 or 102 < nx or 102 < ny:continue
+            if arr[nx][ny] == 1 and visited[nx][ny] == 1:
+                visited[nx][ny] += visited[x][y]
+                que.append((nx, ny))
+    
+    
     return answer
