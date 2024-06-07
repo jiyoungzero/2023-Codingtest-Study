b5ffabe882de5b3088b4800e2ca343c35e007114
이지영 <wldudwldhd@naver.com>
2024-06-07 23:21:17 +0900

    [PGS]게임맵최단거리/240607/복습

--- jiyoung풀이/PGS문제/pgs_타겟넘버.py
+++ jiyoung풀이/PGS문제/pgs_타겟넘버.py
@@ -1,3 +1,16 @@
+def dfs(nums, target, depth, cur):
+    result = 0
+    if depth == len(nums):
+        if cur == target:
+            return 1
+        else: return 0
+    result += dfs(nums, target, depth+1, cur+nums[depth])
+    result += dfs(nums, target, depth+1, cur-nums[depth])
+    
+    return result 
+
 def solution(numbers, target):
     answer = 0
-    return answer 
+    answer = dfs(numbers, target, 0, 0)
+    
+    return answer
