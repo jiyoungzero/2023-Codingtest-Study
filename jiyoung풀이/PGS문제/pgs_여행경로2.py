7d23927347df444b0e929a61834246d7d3b8bf41
이지영 <wldudwldhd@naver.com>
2024-06-12 00:43:37 +0900

    [PGS]여행경로/240611/실패

--- jiyoung풀이/PGS문제/pgs_여행경로2.py
+++ jiyoung풀이/PGS문제/pgs_여행경로2.py
@@ -1,9 +1,20 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 def solution(tickets):
     answer = []
     graph = defaultdict(list)
+    tickets.sort()
+    
     for s, e in tickets:
         graph[s].append(e)
         
     print(graph)
+    que = deque()
+    que.append('ICN')
+    while que:
+        now = que.popleft()
+        answer.append(now)
+        
+        if graph[now]:
+            que.append(graph[now][0])
+            graph[now] = graph[now][1:]
     return answer
