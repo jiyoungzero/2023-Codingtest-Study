28433f8d836102d4ccba138eb5321a33f032dc9d
이지영 <wldudwldhd@naver.com>
2024-06-13 22:49:54 +0900

    [PGS]여행경로/240613/BFS풀이

--- jiyoung풀이/PGS문제/pgs_여행경로2.py
+++ jiyoung풀이/PGS문제/pgs_여행경로2.py
@@ -23,3 +23,26 @@ def solution(tickets):
         
     print(answer)
     return answer[::-1]
+
+
+# BFS 풀이
+from collections import deque
+def solution(tickets):
+    answer = []
+    q = deque()
+    q.append(("ICN",["ICN"], []))
+    
+    while q:
+        airport, path, used = q.popleft()
+
+        if len(used) == len(tickets):
+            answer.append(path)
+        
+        for idx, ticket in enumerate(tickets):
+            if ticket[0] == airport and not idx in used:
+                q.append((ticket[1], path+[ticket[1]], used+[idx]))
+                
+    
+    answer.sort()
+
+    return answer[0]
