265e351c461c5c9e63ca361c562a842d3fd59fbc
이지영 <wldudwldhd@naver.com>
2024-06-22 16:09:34 +0900

    [PGS]붕대감기/240622/80점/부분실패

--- jiyoung풀이/PGS문제/pgs_붕대감기.py
+++ jiyoung풀이/PGS문제/pgs_붕대감기.py
@@ -6,20 +6,31 @@ def solution(bandage, health, attacks):
     attacks = deque(attacks)
 
     while attacks:
-        # 해당 while(time + bandage[0] < at) 돌리면서 now_health보정
         at, attack = attacks.popleft()
         
-        if time + bandage[0] < at:
-            now_health = min(now_health + bandage[0]*bandage[1] + bandage[2], health)
-            time += bandage[0]
-        
-        else:
-            if time+bandage[0] == at:
-                now_health = min(now_health + bandage[0]*bandage[1] + bandage[2], health)
-            else: now_health = min(now_health + time+bandage[0] - at - 1, health)
+        if time + bandage[0] == at:
+            now_health = min(now_health + (bandage[0]-1)*bandage[1], health)
             now_health -= attack
             time = at
-        print(now_health)
+            
+        elif time+bandage[0] < at:
+            while time + bandage[0] < at:
+                time += bandage[0]
+                now_health = min(now_health + bandage[0]*bandage[1]+bandage[2], health)
+            time = at
+            now_health = min(health, now_health + (at-time)*bandage[1])
+            now_health -= attack
+            
+        
+        else: # time+bandage[0] > at 일 때
+            now_health = min(now_health + (at - time-1)*bandage[1], health)
+            now_health -= attack
+            time = at 
 
-    
-    return answer
+
+        # print(now_health)
+    print(now_health)
+    if now_health <= 0:
+        return -1
+    else:
+        return now_health
