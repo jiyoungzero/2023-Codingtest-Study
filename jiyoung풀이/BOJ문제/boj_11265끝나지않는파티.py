e6cc6b3e7e7e8e0795c94502d673c202898a1989
이지영 <wldudwldhd@naver.com>
2024-06-24 15:33:26 +0900

    [BOJ]끝나지않는파티/240624/플로이드워셜/다시

--- jiyoung풀이/BOJ문제/boj_11265끝나지않는파티.py
+++ jiyoung풀이/BOJ문제/boj_11265끝나지않는파티.py
@@ -1,3 +1,27 @@
 import sys
 input = sys.stdin.readline 
 
+n, m = map(int ,input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+requests = []
+for _ in range(m):
+    requests.append(list(map(int, input().split())))
+    
+distance = [[int(1e9)]*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        distance[i][j] = matrix[i][j]
+    
+for k in range(n):
+    for a in range(n):
+        for b in range(n):
+            distance[a][b] = min(distance[a][b], distance[a][k]+distance[k][b])
+
+
+for req in requests:
+    a, b, c= req
+    if distance[a-1][b-1] <= c:
+        print("Enjoy other party")
+    else:
+        print("Stay here")    
