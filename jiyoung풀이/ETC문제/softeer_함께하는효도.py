3b92e96773a98c90f03a047e11c3df739e1f7663
이지영 <wldudwldhd@naver.com>
2024-06-24 20:53:50 +0900

    [ETC]함께하는효도/240624/성공

--- jiyoung풀이/ETC문제/softeer_함께하는효도.py
+++ jiyoung풀이/ETC문제/softeer_함께하는효도.py
@@ -1,53 +1,64 @@
 # Lv. 3
-
+from itertools import product
 import sys
 input = sys.stdin.readline 
-from collections import deque
 
-dxs, dys = [0,0,1,-1],[1,-1,0,0]
+
+
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 friends = [tuple(map(int, input().split())) for _ in range(m)]
-
 result = 0
+
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-def bfs(sx, sy):
-    que = deque()
-    # print(visited[sx][sy])
-    visited[sx][sy][0] = arr[sx][sy]
-    que.append((sx, sy))
+def get_result(paths):
+    tmp = 0
+    visited = [[False]*n for _ in range(n)]
+    dxs, dys = [0,0,1,-1],[1,-1,0,0] # 아래, 위, 오른쪽, 왼쪽
+
     
-    while que:
-        x, y = que.popleft()
-        now_time = visited[x][y][1]
-        if now_time > 3:continue
+    for i, friend in enumerate(friends):
+        x, y = friend
+        x -= 1
+        y -= 1
+        tmp += arr[x][y]
+        visited[x][y] = True
         
-        for k in range(4):
+        for path in paths:
+            # print(paths, path)
+            k = path[i]
             nx, ny = x + dxs[k], y + dys[k]
-            if not in_range(nx, ny):continue
-            nxt_time = visited[nx][ny][1]
-            if now_time > nxt_time:continue
-            else:
-                visited[nx][ny][0] = max(visited[nx][ny][0], visited[x][y][0] + arr[nx][ny])
-                visited[nx][ny][1] += 1
-                que.append((nx, ny))
+            if not in_range(nx,ny): return -1 # 격자 밖 -> 최대의 경우가 될 수 없음
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                tmp += arr[nx][ny]
+            x, y = nx, ny
         
+    return tmp
 
 
-for friend in friends:
-    x, y = friend
-    visited = [[[0,0] for _ in range(n)] for _ in range(n)] # 수확량, 시간
-
-    bfs(x, y)
+# [0초일때 m명의 경로], [1초일때], [2초일때], [3초일때] 
+def get_path(path): # path = [[0,0,1,2],[0,0,2,3],[2,3,1,0]]
+    global result, possible_path
     
-    for row in visited:
-        max_val = 0
-        for v, _ in row:
-            max_val = max(max_val, v)
-        result += max_val
-        
+    p_len = len(possible_path)
+    # 0~3까지 선택 
+    if len(path) == 3:
+        result = max(result, get_result(path))
+        return 
+    
+    for i in range(p_len):
+        path.append(possible_path[i])
+        get_path(path)
+        path.pop()
+
+
+
+possible_path = list(product(list(range(4)), repeat = m))
+get_path([])
 print(result)
+
+    
