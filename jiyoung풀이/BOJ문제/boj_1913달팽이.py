a16ce45b428fd13c97544d70d9f33bfa53cbf10a
이지영 <wldudwldhd@naver.com>
2024-07-02 19:28:35 +0900

    [BOJ]달팽이/240702/성공/내풀이

--- jiyoung풀이/BOJ문제/boj_1913달팽이.py
+++ jiyoung풀이/BOJ문제/boj_1913달팽이.py
@@ -9,15 +9,15 @@ x, y = n//2, n//2
 dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 cnt = 0
 arr[x][y] = 1
-answer = [0, 0]
+answer = [x, y]
 
 def can_go(sx, sy):
     if sx < 0 or sy < 0 or n <= sx or n <= sy: return False
     if arr[sx][sy] > 0:return False
     return True
 
-while arr[0][0] == 0:
-    print(x, y)
+while True:
+    if (x, y) == (0,0):break
     for k in range(4):
         if k == 0 or k == 2:
             cnt += 1
@@ -25,12 +25,14 @@ while arr[0][0] == 0:
             nx, ny = x + dxs[k], y + dys[k]
             if can_go(nx, ny):
                 arr[nx][ny] = arr[x][y] + 1
-                if arr[nx][ny] == target:
-                    answer = [nx+1, ny+1]
+
                 x, y = nx, ny
                 
-
-for row in arr:
-    print(*row)
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == target:
+            answer = [i, j]
+        print(arr[i][j], end = " ")
+    print()
 print(*answer)
         
